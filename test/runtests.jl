eb05b8de560ff730fdfe5e89acf2559735d2c4ce
GenXProject <84470580+GenXProject@users.noreply.github.com>
2021-06-04 04:13:18 -0600

    Attempt to activate Travis Build

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,5 +21,5 @@ using Test
     @test simple_operation(2.0, 3.0) == 5
     @test simple_operation(2.1, 3.1)==5.2
     @test simple_operation(21.0, 31.0)== 52.0
-    @test simple_operation(73.0, 34.0)== 107.0
+    @test simple_operation(73.0, 35.0)== 108.0
 end
