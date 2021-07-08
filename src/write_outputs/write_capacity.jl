81f6987a48e7152d2e1a3c40f03e8925d31f5a8f
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2021-07-08 12:40:24 +0200

    Update write_capacity.jl
    Added missing period in doc string.

--- src/write_outputs/write_capacity.jl
+++ src/write_outputs/write_capacity.jl
@@ -17,7 +17,7 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 @doc raw"""
 	write_capacity(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, EP::Model))
 
-Function for writing the diferent capacities for the different generation technologies (starting capacities or, existing capacities, retired capacities, and new-built capacities)
+Function for writing the diferent capacities for the different generation technologies (starting capacities or, existing capacities, retired capacities, and new-built capacities).
 """
 function write_capacity(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 	# Capacity decisions
