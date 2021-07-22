a7333a11f1e1cc249fc0f422c567d575e166e81e
sambuddhac <sambuddha.chakrabarti@gmail.com>
2021-07-22 12:42:06 -0600

    Modified the run file

--- Example_Systems/SmallNewEngland/OneZone/Run.jl
+++ Example_Systems/SmallNewEngland/OneZone/Run.jl
@@ -16,10 +16,10 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 
 cd(dirname(@__FILE__))
 settings_path = joinpath(pwd(), "Settings")
-#=
+
 environment_path = "../../../package_activate.jl"
 include(environment_path) #Run this line to activate the Julia virtual environment for GenX; skip it, if the appropriate package versions are installed
-=#
+
 ### Set relevant directory paths
 src_path = "../../../src/"
 
