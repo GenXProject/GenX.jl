c063875166eea013604ed33d7560a113aea2105c
Fangwei Cheng <fc4uk@virginia.edu>
2022-01-25 18:28:13 -0500

    Modify fleccs2- solvent storage system
    Add aux boiler, steam from mid -pressure steam turbine is used to regenerate solvent.

--- src/GenX.jl
+++ src/GenX.jl
@@ -49,13 +49,13 @@ using Statistics
 using DelimitedFiles
 
 # Uncomment if Gurobi or CPLEX active license and installations are there and the user intends to use either of them
-#using Gurobi
+using Gurobi
 #using CPLEX
 #using MOI
-using SCIP
-using BenchmarkTools
-using Clp
-using Cbc
+#using SCIP
+#using BenchmarkTools
+#using Clp
+#using Cbc
 
 # Global scaling factor used when ParameterScale is on to shift values from MW to GW
 # DO NOT CHANGE THIS (Unless you do so very carefully)
