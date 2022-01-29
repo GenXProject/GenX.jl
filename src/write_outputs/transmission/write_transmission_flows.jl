b1a37260760ee39644382160bf1a08150cacea13
Jacob Schwartz <jacob@jaschwartz.net>
2022-01-28 22:22:28 -0500

    Fix method call and argument order

--- src/write_outputs/transmission/write_transmission_flows.jl
+++ src/write_outputs/transmission/write_transmission_flows.jl
@@ -14,7 +14,7 @@ in LICENSE.txt.  Users uncompressing this from an archive may not have
 received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-function write_transmission_flows(path::AbstractString, setup::Dict, inputs::Dict, EP::Model)
+function write_transmission_flows(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 	# Transmission related values
 	T = inputs["T"]     # Number of time steps (hours)
 	Z = inputs["Z"]     # Number of zones
