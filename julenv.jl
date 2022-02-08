0d46acc43b7ffc105ffdc9d7245b1f72d5c6f9bf
Qingyu Xu <qingyux@princeton.edu>
2022-02-08 13:50:57 -0500

    Remove cbc

--- julenv.jl
+++ julenv.jl
@@ -18,7 +18,7 @@ import Pkg
 using Pkg
 Pkg.activate("GenXJulEnv")
 
-Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.7.1")) # 0.8.0
+# Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.7.1")) # 0.8.0
 Pkg.add(Pkg.PackageSpec(name="Clp", version="0.8.4"))
 Pkg.add(Pkg.PackageSpec(name="DataStructures", version="0.18.9"))
 Pkg.add(Pkg.PackageSpec(name="Dates"))
@@ -77,7 +77,7 @@ Pkg.add(Pkg.PackageSpec(name="LinearAlgebra"))
 	Pkg.add(Pkg.PackageSpec(name="YAML", version="0.4.3"))
 	Pkg.add(Pkg.PackageSpec(name="LinearAlgebra"))
 elseif v"1.5" <= VERSION < v"1.7"=#
-	Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.7.1"))
+	# Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.7.1"))
 	Pkg.add(Pkg.PackageSpec(name="Clp", version="0.8.4"))
 	Pkg.add(Pkg.PackageSpec(name="DataStructures", version="0.18.9"))
 	Pkg.add(Pkg.PackageSpec(name="Dates"))
