ab20e853d429d5325e7f6b1a0c8081d30cc1ebdb
Jacob Schwartz <jacob@jaschwartz.net>
2022-02-13 20:39:41 -0500

    Vectorize outputs
    write_capacity_value & write_reserve_margin_revenue
    
    The column output order here is slightly different, but the values are
    the same.
    
    Co-authored-by: Qingyu Xu <xuqingyu0610@gmail.com>

--- src/write_outputs/capacity_reserve_margin/write_capacity_value.jl
+++ src/write_outputs/capacity_reserve_margin/write_capacity_value.jl
@@ -14,7 +14,7 @@ in LICENSE.txt.  Users uncompressing this from an archive may not have
 received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-function write_capacity_value(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, dfPower::DataFrame, dfCharge::DataFrame, dfResMar::DataFrame, dfCap::DataFrame)
+function write_capacity_value(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 	dfGen = inputs["dfGen"]
 	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
 	T = inputs["T"]     # Number of time steps (hours)
@@ -22,45 +22,47 @@ function write_capacity_value(path::AbstractString, sep::AbstractString, inputs:
 	Z = inputs["Z"]     # Number of zonests
 	L = inputs["L"] # Number of lines
 	THERM_ALL = inputs["THERM_ALL"]
-	# VRE_HYDRO_RES = union(inputs["HYDRO_RES"],inputs["VRE"])
 	VRE = inputs["VRE"]
 	HYDRO_RES = inputs["HYDRO_RES"]
 	STOR_ALL = inputs["STOR_ALL"]
 	FLEX = inputs["FLEX"]
 	MUST_RUN = inputs["MUST_RUN"]
-	#calculating capacity value under reserve margin constraint, added by NP on 10/21/2020
+	if setup["ParameterScale"] == 1
+		existingplant_position = findall(x -> x >= 1, (value.(EP[:eTotalCap])) * ModelScalingFactor)
+	else
+		existingplant_position = findall(x -> x >= 1, (value.(EP[:eTotalCap])))
+	end
+	THERM_ALL_EX = intersect(THERM_ALL, existingplant_position)
+	VRE_EX = intersect(VRE, existingplant_position)
+	HYDRO_RES_EX = intersect(HYDRO_RES, existingplant_position)
+	STOR_ALL_EX = intersect(STOR_ALL, existingplant_position)
+	FLEX_EX = intersect(FLEX, existingplant_position)
+	MUST_RUN_EX = intersect(MUST_RUN, existingplant_position)
+	totalcap = repeat((value.(EP[:eTotalCap])), 1, T)
 	dfCapValue = DataFrame()
 	for i in 1:inputs["NCapacityReserveMargin"]
-		dfCapValue_ = dfPower[1:end-1,:]
-		dfCapValue_ = select!(dfCapValue_, Not(:AnnualSum))
-		if v"1.3" <= VERSION < v"1.4"
-			dfCapValue_[!,:Reserve] .= Symbol("CapRes_$i")
-		elseif v"1.4" <= VERSION < v"1.7"
-			#dfCapValue_.Reserve = Symbol("CapRes_$i")
-			dfCapValue_.Reserve = fill(Symbol("CapRes_$i"), size(dfCapValue_, 1))
+		temp_dfCapValue = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!, :Zone], Reserve = fill(Symbol("CapRes_$i"), G))
+		temp_capvalue = zeros(G, T)
+		temp_riskyhour = zeros(G, T)
+		temp_cap_derate = zeros(G, T)
+		if setup["ParameterScale"] == 1
+			riskyhour_position = findall(x -> x >= 1, ((dual.(EP[:cCapacityResMargin][i, :])) ./ inputs["omega"] * ModelScalingFactor))
+		else
+			riskyhour_position = findall(x -> x >= 1, ((dual.(EP[:cCapacityResMargin][i, :])) ./ inputs["omega"]))
 		end
-		for t in 1:T
-			if dfResMar[i,t] > 0.0001
-				for y in 1:G
-					if (dfCap[y,:EndCap] > 0.0001) .& (y in STOR_ALL) # including storage
-						dfCapValue_[y,Symbol("t$t")] = ((dfPower[y,Symbol("t$t")]-dfCharge[y,Symbol("t$t")]) * dfGen[y,Symbol("CapRes_$i")])/dfCap[y,:EndCap]
-					elseif (dfCap[y,:EndCap] > 0.0001) .& (y in HYDRO_RES) # including hydro and VRE
-						dfCapValue_[y,Symbol("t$t")] = ((dfPower[y,Symbol("t$t")]) * dfGen[y,Symbol("CapRes_$i")])/dfCap[y,:EndCap]
-					elseif (dfCap[y,:EndCap] > 0.0001) .& (y in VRE) # including hydro and VRE
-						dfCapValue_[y,Symbol("t$t")] = ((inputs["pP_Max"][y,t]) * dfGen[y,Symbol("CapRes_$i")])
-					elseif (dfCap[y,:EndCap] > 0.0001) .& (y in FLEX) # including flexible load
-						dfCapValue_[y,Symbol("t$t")] = ((dfCharge[y,Symbol("t$t")] - dfPower[y,Symbol("t$t")]) * dfGen[y,Symbol("CapRes_$i")])/dfCap[y,:EndCap]
-					elseif (dfCap[y,:EndCap] > 0.0001) .& (y in THERM_ALL) # including thermal
-						dfCapValue_[y,Symbol("t$t")] = dfGen[y,Symbol("CapRes_$i")]
-					elseif (dfCap[y,:EndCap] > 0.0001) .& (y in MUST_RUN) # Must run technologies are not considered for reserve margin
-						dfCapValue_[y,Symbol("t$t")] = dfGen[y,Symbol("CapRes_$i")]
-					end
-				end
-			else
-				dfCapValue_[!,Symbol("t$t")] .= 0
-			end
-		end
-		dfCapValue = vcat(dfCapValue, dfCapValue_)
+		temp_riskyhour[:, riskyhour_position] = ones(Int, G, length(riskyhour_position))
+		temp_cap_derate[existingplant_position, :] = repeat(dfGen[existingplant_position, Symbol("CapRes_$i")], 1, T)
+
+		temp_capvalue[THERM_ALL_EX, :] = temp_cap_derate[THERM_ALL_EX, :] .* temp_riskyhour[THERM_ALL_EX, :]
+		temp_capvalue[VRE_EX, :] = temp_cap_derate[VRE_EX, :] .* (inputs["pP_Max"][VRE_EX, :]) .* temp_riskyhour[VRE_EX, :]
+		temp_capvalue[MUST_RUN_EX, :] = temp_cap_derate[MUST_RUN_EX, :] .* (inputs["pP_Max"][MUST_RUN_EX, :]) .* temp_riskyhour[MUST_RUN_EX, :]
+		temp_capvalue[HYDRO_RES_EX, :] = temp_cap_derate[HYDRO_RES_EX, :] .* (value.(EP[:vP][HYDRO_RES_EX, :])) .* temp_riskyhour[HYDRO_RES_EX, :] ./ totalcap[HYDRO_RES_EX, :]
+		temp_capvalue[STOR_ALL_EX, :] = temp_cap_derate[STOR_ALL_EX, :] .* ((value.(EP[:vP][STOR_ALL_EX, :]) - value.(EP[:vCHARGE][STOR_ALL_EX, :]).data)) .* temp_riskyhour[STOR_ALL_EX, :] ./ totalcap[STOR_ALL_EX, :]
+		temp_capvalue[FLEX_EX, :] = temp_cap_derate[FLEX_EX, :] .* ((value.(EP[:vCHARGE_FLEX][FLEX_EX, :]).data - value.(EP[:vP][FLEX_EX, :]))) .* temp_riskyhour[FLEX_EX, :] ./ totalcap[FLEX_EX, :]
+		temp_dfCapValue = hcat(temp_dfCapValue, DataFrame(temp_capvalue, :auto))
+		auxNew_Names = [Symbol("Resource"); Symbol("Zone"); Symbol("Reserve"); [Symbol("t$t") for t in 1:T]]
+		rename!(temp_dfCapValue, auxNew_Names)
+		append!(dfCapValue, temp_dfCapValue)
 	end
-	CSV.write(string(path,sep,"CapacityValue.csv"),dfCapValue)
+	CSV.write(string(path,sep,"CapacityValue.csv"), dfCapValue)
 end
--- src/write_outputs/capacity_reserve_margin/write_reserve_margin_revenue.jl
+++ src/write_outputs/capacity_reserve_margin/write_reserve_margin_revenue.jl
@@ -17,55 +17,37 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 @doc raw"""
 	write_reserve_margin_revenue(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, dfPower::DataFrame, dfCharge::DataFrame, dfResMar::DataFrame, dfCap::DataFrame)
 
-Function for reporting the capacity revenue earned by each generator listed in the input file. GenX will print this file only when capacity reserve margin is modeled and the shadow price can be obtained form the solver. Each row corresponds to a generator, and each column starting from the 6th to the second last is the total revenue from each capacity reserve margin constraint. The revenue is calculated as the capacity contribution of each time steps multiplied by the shadow price, and then the sum is taken over all modeled time steps. The last column is the total revenue received from all capacity reserve margin constraints.  As a reminder, GenX models the capacity reserve margin (aka capacity market) at the time-dependent level, and each constraint either stands for an overall market or a locality constraint.
+Function for reporting the capacity revenue earned by each generator listed in the input file. 
+    GenX will print this file only when capacity reserve margin is modeled and the shadow price can be obtained form the solver. 
+    Each row corresponds to a generator, and each column starting from the 6th to the second last is the total revenue from each capacity reserve margin constraint. 
+    The revenue is calculated as the capacity contribution of each time steps multiplied by the shadow price, and then the sum is taken over all modeled time steps. 
+    The last column is the total revenue received from all capacity reserve margin constraints.  
+    As a reminder, GenX models the capacity reserve margin (aka capacity market) at the time-dependent level, and each constraint either stands for an overall market or a locality constraint.
 """
-function write_reserve_margin_revenue(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, dfPower::DataFrame, dfCharge::DataFrame, dfResMar::DataFrame, dfCap::DataFrame)
+function write_reserve_margin_revenue(path::AbstractString, sep::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 	dfGen = inputs["dfGen"]
 	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
-	T = inputs["T"]     # Number of time steps (hours)
-	SEG = inputs["SEG"]  # Number of lines
-	Z = inputs["Z"]     # Number of zonests
-	L = inputs["L"] # Number of lines
 	THERM_ALL = inputs["THERM_ALL"]
 	VRE = inputs["VRE"]
 	HYDRO_RES = inputs["HYDRO_RES"]
-	# VRE_HYDRO_RES = union(inputs["HYDRO_RES"],inputs["VRE"])
 	STOR_ALL = inputs["STOR_ALL"]
 	FLEX = inputs["FLEX"]
-	### calculating capacity reserve revenue
-
-	dfResRevenue = DataFrame(region = dfGen[!,:region], Resource = inputs["RESOURCES"], zone = dfGen[!,:Zone], Cluster = dfGen[!,:cluster], R_ID = dfGen[!,:R_ID])
+	MUST_RUN = inputs["MUST_RUN"]
+	dfResRevenue = DataFrame(Region = dfGen[!, :region], Resource = inputs["RESOURCES"], Zone = dfGen[!, :Zone], Cluster = dfGen[!, :cluster], R_ID=dfGen[!, :R_ID], AnnualSum = zeros(G))
 	for i in 1:inputs["NCapacityReserveMargin"]
-		# initiate the process by assuming everything is thermal
-		dfResRevenue = hcat(dfResRevenue, round.(Int, dfCap[1:end-1,:EndCap] .* dfGen[!,Symbol("CapRes_$i")] .* sum(dfResMar[i,:])))
-		for y in 1:G
-			if (y in STOR_ALL)
-				dfResRevenue[y,:x1] = round.(Int, sum(
-				(DataFrame([[names(dfPower)]; collect.(eachrow(dfPower))], [:column; Symbol.(axes(dfPower, 1))])[4:T+3,y+1] .-
-				DataFrame([[names(dfPower)]; collect.(eachrow(dfCharge))], [:column; Symbol.(axes(dfPower, 1))])[4:T+3,y+1]) .*
-				DataFrame([[names(dfResMar)]; collect.(eachrow(dfResMar))], [:column; Symbol.(axes(dfResMar, 1))])[!,i+1] .* dfGen[y,Symbol("CapRes_$i")]))
-			elseif (y in HYDRO_RES)
-				dfResRevenue[y,:x1] = round.(Int, sum((DataFrame([[names(dfPower)]; collect.(eachrow(dfPower))], [:column; Symbol.(axes(dfPower, 1))])[4:T+3,y+1]) .*
-				DataFrame([[names(dfResMar)]; collect.(eachrow(dfResMar))], [:column; Symbol.(axes(dfResMar, 1))])[!,i+1] .* dfGen[y,Symbol("CapRes_$i")]))
-           		elseif (y in VRE)
-                		dfResRevenue[y,:x1] = round.(Int, sum( dfCap[y,:EndCap] .* inputs["pP_Max"][y,:] .*
-                		DataFrame([[names(dfResMar)]; collect.(eachrow(dfResMar))], [:column; Symbol.(axes(dfResMar, 1))])[!,i+1] .* dfGen[y,Symbol("CapRes_$i")]))
-			elseif (y in FLEX)
-				dfResRevenue[y,:x1] = round.(Int, sum(
-				(DataFrame([[names(dfPower)]; collect.(eachrow(dfCharge))], [:column; Symbol.(axes(dfPower, 1))])[4:T+3,y+1] .-
-				DataFrame([[names(dfPower)]; collect.(eachrow(dfPower))], [:column; Symbol.(axes(dfPower, 1))])[4:T+3,y+1]) .*
-				DataFrame([[names(dfResMar)]; collect.(eachrow(dfResMar))], [:column; Symbol.(axes(dfResMar, 1))])[!,i+1] .* dfGen[y,Symbol("CapRes_$i")]))
-			end
+		tempresrev = zeros(G)
+		tempresrev[THERM_ALL] = dfGen[THERM_ALL, Symbol("CapRes_$i")] .* (value.(EP[:eTotalCap][THERM_ALL])) * sum(dual.(EP[:cCapacityResMargin][i, :]))
+		tempresrev[VRE] = dfGen[VRE, Symbol("CapRes_$i")] .* (value.(EP[:eTotalCap][VRE])) .* (inputs["pP_Max"][VRE, :] * (dual.(EP[:cCapacityResMargin][i, :])))
+		tempresrev[MUST_RUN] = dfGen[MUST_RUN, Symbol("CapRes_$i")] .* (value.(EP[:eTotalCap][MUST_RUN])) .* (inputs["pP_Max"][MUST_RUN, :] * (dual.(EP[:cCapacityResMargin][i, :])))
+		tempresrev[HYDRO_RES] = dfGen[HYDRO_RES, Symbol("CapRes_$i")] .* (value.(EP[:vP][HYDRO_RES, :]) * (dual.(EP[:cCapacityResMargin][i, :])))
+		tempresrev[STOR_ALL] = dfGen[STOR_ALL, Symbol("CapRes_$i")] .* ((value.(EP[:vP][STOR_ALL, :]) - value.(EP[:vCHARGE][STOR_ALL, :]).data) * (dual.(EP[:cCapacityResMargin][i, :])))
+		tempresrev[FLEX] = dfGen[FLEX, Symbol("CapRes_$i")] .* ((value.(EP[:vCHARGE_FLEX][FLEX, :]).data - value.(EP[:vP][FLEX, :])) * (dual.(EP[:cCapacityResMargin][i, :])))
+		if setup["ParameterScale"] == 1
+			tempresrev *= ModelScalingFactor^2
 		end
-		# the capacity and power is in MW already, no need to scale
-		# if setup["ParameterScale"] == 1
-		# 	dfResRevenue[!,:x1] = dfResRevenue[!,:x1] * ModelScalingFactor #(1e+3) # This is because although the unit of price is US$/MWh, the capacity or generation is in GW
-		# end
-		rename!(dfResRevenue, Dict(:x1 => Symbol("CapRes_$i")))
+		dfResRevenue.AnnualSum .+= tempresrev
+		dfResRevenue = hcat(dfResRevenue, DataFrame([tempresrev], [Symbol("CapRes_$i")]))
 	end
-	dfResRevenue.AnnualSum = sum(eachcol(dfResRevenue[:,6:inputs["NCapacityReserveMargin"]+5]))
-
-
 	CSV.write(string(path,sep,"ReserveMarginRevenue.csv"), dfResRevenue)
 	return dfResRevenue
 end
