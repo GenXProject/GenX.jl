f88fa9e4719247689b9bb73662a10d28400c9c33
Jacob Schwartz <jacob@jaschwartz.net>
2022-02-21 11:07:55 -0500

    Vectorize write_reserve_margin_revenue
    Co-authored-by: Qingyu Xu <xuqingyu0610@gmail.com>

--- src/write_outputs/write_outputs.jl
+++ src/write_outputs/write_outputs.jl
@@ -174,7 +174,7 @@ function write_outputs(EP::Model, path::AbstractString, setup::Dict, inputs::Dic
 			elapsed_time_rsv_margin = @elapsed write_reserve_margin_w(path, sep, inputs, setup, EP)
 		  println("Time elapsed for writing reserve margin is")
 		  println(elapsed_time_rsv_margin)
-			dfResRevenue = write_reserve_margin_revenue(path, sep, inputs, setup, dfPower, dfCharge, dfResMar, dfCap)
+			dfResRevenue = write_reserve_margin_revenue(path, sep, inputs, setup, EP)
 			elapsed_time_cap_value = @elapsed write_capacity_value(path, sep, inputs, setup, dfPower, dfCharge, dfResMar, dfCap)
 		  println("Time elapsed for writing capacity value is")
 		  println(elapsed_time_cap_value)
