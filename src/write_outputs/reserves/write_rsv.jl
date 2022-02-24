172630ce3f39d3fff39c5dc06031fc7af241243d
Jacob Schwartz <jacob@jaschwartz.net>
2022-02-24 17:45:34 -0500

    Annualize outputs for reserves and ucommit
    The period weighting ω is now taken into account for sums of reg and rsv
    values, starts and shutdowns.
    
    In flows and tlosses, the change from Sum→AnnualSum is for consistent
    terminology.
    
    Co-authored-by: Qingyu Xu <xuqingyu0610@gmail.com>

--- src/write_outputs/reserves/write_rsv.jl
+++ src/write_outputs/reserves/write_rsv.jl
@@ -21,18 +21,18 @@ function write_rsv(path::AbstractString, sep::AbstractString, inputs::Dict, setu
 	T = inputs["T"]     # Number of time steps (hours)
 	RSV = inputs["RSV"]
 
-	dfRsv = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!, :Zone], Sum = Array{Float64}(undef, G))
+	dfRsv = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!, :Zone])
 	rsv = zeros(G,T)
 	unmet_vec = zeros(T)
 	rsv[RSV, :] = value.(EP[:vRSV][RSV, :])
 	unmet_vec[RSV] = value.(EP[:vUNMET_RSV][RSV])
 	total_unmet = sum(unmet_vec)
-	dfRsv.Sum .= sum(rsv, dims=2)[:]
+	dfRsv.AnnualSum = rsv * inputs["omega"]
 	dfRsv = hcat(dfRsv, DataFrame(rsv, :auto))
-	auxNew_Names=[Symbol("Resource");Symbol("Zone");Symbol("Sum");[Symbol("t$t") for t in 1:T]]
+	auxNew_Names=[Symbol("Resource");Symbol("Zone");Symbol("AnnualSum");[Symbol("t$t") for t in 1:T]]
 	rename!(dfRsv,auxNew_Names)
 
-	total = DataFrame(["Total" 0 sum(dfRsv[!,:Sum]) zeros(1, T)], :auto)
+	total = DataFrame(["Total" 0 sum(dfRsv.AnnualSum) zeros(1, T)], :auto)
 	unmet = DataFrame(["unmet" 0 total_unmet zeros(1, T)], :auto)
 	total[!, 4:T+3] .= sum(rsv, dims = 1)
 	unmet[!, 4:T+3] .= transpose(unmet_vec)
