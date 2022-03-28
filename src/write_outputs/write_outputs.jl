2026e54c84ab958ac290527a44f6c17dcbcd360d
jfmorris <jfmorris@mit.edu>
2022-03-28 14:45:21 -0400

    TEMPORARY remove write_storagedual()
    Didn't work in storage-free example

--- src/write_outputs/write_outputs.jl
+++ src/write_outputs/write_outputs.jl
@@ -107,9 +107,9 @@ function write_outputs(EP::Model, path::AbstractString, setup::Dict, inputs::Dic
 		elapsed_time_reliability = @elapsed write_reliability(path, sep, inputs, setup, EP)
 		println("Time elapsed for writing reliability is")
 		println(elapsed_time_reliability)
-		elapsed_time_stordual = @elapsed write_storagedual(path, sep, inputs, setup, EP)
-		println("Time elapsed for writing storage duals is")
-		println(elapsed_time_stordual)
+		### elapsed_time_stordual = @elapsed write_storagedual(path, sep, inputs, setup, EP)
+		### println("Time elapsed for writing storage duals is")
+		### println(elapsed_time_stordual)
 	end
 
 	if setup["UCommit"] >= 1
