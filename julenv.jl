a7357bc744656149dc2a3cac81426952c155e581
sambuddhac <sambuddha.chakrabarti@gmail.com>
2022-03-29 17:40:52 -0400

    Updated gitignore to account for all Results folder and GenXJulEnv folders, Updated documentation, and compatible package version with a bump to Gurobi

--- julenv.jl
+++ julenv.jl
@@ -29,50 +29,27 @@ Pkg.add("MathOptInterface")
 Pkg.add(Pkg.PackageSpec(name="SCIP"))
 Pkg.build("SCIP")
 Pkg.add(Pkg.PackageSpec(name="HiGHS"))
+############################################################################################
+#Uncomment either of the following two lines for the particular version of CPLEX.jl desired
+############################################################################################
+#Pkg.add(Pkg.PackageSpec(name="CPLEX", version="0.6.1"))
 #Pkg.add(Pkg.PackageSpec(name="CPLEX", version="0.7.7"))
+############################################################################################
+#Uncomment either of the following two lines for the particular version of Gurobi.jl desired
+############################################################################################
+#Pkg.add(Pkg.PackageSpec(name="Gurobi", version="0.10.3"))
+#Pkg.add(Pkg.PackageSpec(name="Gurobi", version="0.9.14"))
 Pkg.add(Pkg.PackageSpec(name="CSV", version="0.8.5"))
 Pkg.add(Pkg.PackageSpec(name="Clustering", version="0.14.2"))
 Pkg.add(Pkg.PackageSpec(name="Combinatorics", version="1.0.2"))
 Pkg.add(Pkg.PackageSpec(name="Distances", version="0.10.3"))
-Pkg.add(Pkg.PackageSpec(name="DataFrames", version="1.0.0")) #0.20.2
+Pkg.add(Pkg.PackageSpec(name="DataFrames", version="1.0.0"))
 Pkg.add(Pkg.PackageSpec(name="Documenter", version="0.27.3"))
 Pkg.add(Pkg.PackageSpec(name="DocumenterTools", version="0.1.13"))
-#Pkg.add(Pkg.PackageSpec(name="Gurobi", version="0.9.14"))
-#Pkg.build("Gurobi")
-##Add if elseif with Method of Morris for these
 Pkg.add(Pkg.PackageSpec(name="DiffEqSensitivity", version="6.52.1"))
 Pkg.add(Pkg.PackageSpec(name="Statistics"))
 Pkg.add(Pkg.PackageSpec(name="OrdinaryDiffEq", version="5.60.1"))
 Pkg.add(Pkg.PackageSpec(name="QuasiMonteCarlo", version="0.2.3"))
-##Add if elseif with Method of Morris for these
-Pkg.add(Pkg.PackageSpec(name="BenchmarkTools", version="1.1.1"))
-Pkg.add(Pkg.PackageSpec(name="MathProgBase", version="0.7.8"))
-Pkg.add(Pkg.PackageSpec(name="StatsBase", version="0.33.8"))
-Pkg.add(Pkg.PackageSpec(name="YAML", version="0.4.7"))
-Pkg.add(Pkg.PackageSpec(name="LinearAlgebra"))
-Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.8.0"))
-Pkg.add(Pkg.PackageSpec(name="Clp", version="0.8.4"))
-Pkg.add(Pkg.PackageSpec(name="DataStructures", version="0.18.9"))
-Pkg.add(Pkg.PackageSpec(name="Dates"))
-Pkg.add(Pkg.PackageSpec(name="GLPK", version="0.14.12"))
-Pkg.add(Pkg.PackageSpec(name="Ipopt", version="0.7.0"))
-Pkg.add(Pkg.PackageSpec(name="JuMP", version="0.21.8"))
-Pkg.add(Pkg.PackageSpec(name="CPLEX", version="0.6.1"))
-Pkg.add(Pkg.PackageSpec(name="CSV", version="0.8.5"))
-Pkg.add(Pkg.PackageSpec(name="Clustering", version="0.14.2"))
-Pkg.add(Pkg.PackageSpec(name="Combinatorics", version="1.0.2"))
-Pkg.add(Pkg.PackageSpec(name="Distances", version="0.10.3"))
-Pkg.add(Pkg.PackageSpec(name="DataFrames", version="1.0.0")) #0.20.2
-Pkg.add(Pkg.PackageSpec(name="Documenter", version="0.27.3"))
-Pkg.add(Pkg.PackageSpec(name="DocumenterTools", version="0.1.13"))
-#Pkg.add(Pkg.PackageSpec(name="Gurobi", version="0.9.14"))
-#Pkg.build("Gurobi")
-##Add if elseif with Method of Morris for these
-Pkg.add(Pkg.PackageSpec(name="DiffEqSensitivity", version="6.52.1"))
-Pkg.add(Pkg.PackageSpec(name="Statistics", version="1.4.0"))
-Pkg.add(Pkg.PackageSpec(name="OrdinaryDiffEq", version="5.60.1"))
-Pkg.add(Pkg.PackageSpec(name="QuasiMonteCarlo", version="0.2.3"))
-##Add if elseif with Method of Morris for these
 Pkg.add(Pkg.PackageSpec(name="BenchmarkTools", version="1.1.1"))
 Pkg.add(Pkg.PackageSpec(name="MathProgBase", version="0.7.8"))
 Pkg.add(Pkg.PackageSpec(name="StatsBase", version="0.33.8"))
