a79b16596847130ef098f92d210459ef0e38ea2f
Qingyu Xu <qingyux@princeton.edu>
2022-05-17 11:23:38 -0400

    Update ESR module
    1) add writing the load side energy share requirement payment
    2) rewrite the supply side energy share requriement revenue, getting rid of using dfPower and dfESR

--- src/write_outputs/energy_share_requirement/write_esr_revenue.jl
+++ src/write_outputs/energy_share_requirement/write_esr_revenue.jl
@@ -19,20 +19,19 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 
 Function for reporting the renewable/clean credit revenue earned by each generator listed in the input file. GenX will print this file only when RPS/CES is modeled and the shadow price can be obtained form the solver. Each row corresponds to a generator, and each column starting from the 6th to the second last is the total revenue earned from each RPS constraint. The revenue is calculated as the total annual generation (if elgible for the corresponding constraint) multiplied by the RPS/CES price. The last column is the total revenue received from all constraint. The unit is \$.
 """
-function write_esr_revenue(path::AbstractString, inputs::Dict, setup::Dict, dfPower::DataFrame, dfESR::DataFrame)
+function write_esr_revenue(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 	dfGen = inputs["dfGen"]
-	dfESRRev = DataFrame(region = dfGen[!,:region], Resource = inputs["RESOURCES"], zone = dfGen[!,:Zone], Cluster = dfGen[!,:cluster], R_ID = dfGen[!,:R_ID])
+	dfESRRev = DataFrame(Region = dfGen[!,:region], Resource = inputs["RESOURCES"], Zone = dfGen[!,:Zone], Cluster = dfGen[!,:cluster], AnnualSum = zeros(G))
 
 	for i in 1:inputs["nESR"]
-		dfESRRev =  hcat(dfESRRev, dfPower[1:end-1,:AnnualSum] .* dfGen[!,Symbol("ESR_$i")] * dfESR[i,:ESR_Price])
-		# dfpower is in MWh already, price is in $/MWh already, no need to scale
-		# if setup["ParameterScale"] == 1
-		# 	#dfESRRev[!,:x1] = dfESRRev[!,:x1] * (1e+3) # MillionUS$ to US$
-		# 	dfESRRev[!,:x1] = dfESRRev[!,:x1] * ModelScalingFactor # MillionUS$ to US$  # Is this right? -Jack 4/29/2021
-		# end
-		rename!(dfESRRev, Dict(:x1 => Symbol("ESR_$i")))
+	    tempesrrevenue = zeros(G)
+	    tempesrrevenue = (value.(EP[:vP]) * inputs["omega"]) .* dfGen[:, Symbol("ESR_$i")] .* dual.(EP[:cESRShare][i])
+	    if setup["ParameterScale"] == 1
+	        tempesrrevenue *= (ModelScalingFactor^2)
+	    end
+	    dfESRRev.AnnualSum .+= tempesrrevenue
+	    dfESRRev = hcat(dfESRRev, DataFrame([tempesrrevenue], [Symbol("ESR_$i")]))
 	end
-	dfESRRev.AnnualSum = sum(eachcol(dfESRRev[:,6:inputs["nESR"]+5]))
 	CSV.write(joinpath(path, "ESR_Revenue.csv"), dfESRRev)
 	return dfESRRev
 end
