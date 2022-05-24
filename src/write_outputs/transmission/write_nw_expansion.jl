5b70f6f4cec6d8e88211f9191389554cdb799f3c
Qingyu Xu <qingyux@princeton.edu>
2022-05-23 20:05:02 -0400

    Update write_nw_expansion.jl
    So that values are correctly printed.

--- src/write_outputs/transmission/write_nw_expansion.jl
+++ src/write_outputs/transmission/write_nw_expansion.jl
@@ -17,20 +17,23 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 function write_nw_expansion(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
     L = inputs["L"]     # Number of transmission lines
     EXPANSION_LINES = inputs["EXPANSION_LINES"]
-    # Transmission network reinforcements
-    transcap = zeros(L) # Expanded capacity
-    transcapcost = zeros(L) # Expansion Cost
-    transendcap = zeros(L) # Final availability
-    if !isempty(inputs["EXPANSION_LINES"])
-        transcap[EXPANSION_LINES] = value.(EP[:vNEW_TRANS_CAP][EXPANSION_LINES])
-        transcapcost[EXPANSION_LINES] = transendcap .* inputs["pC_Line_Reinforcement"]
-    end
-    transendcap = value.(EP[:eAvail_Trans_Cap])
     dfTransCap = DataFrame(
         Line = 1:L,
-        End_Trans_Capacity = convert(Array{Union{Missing,Float64}}, transendcap),
-        New_Trans_Capacity = convert(Array{Union{Missing,Float64}}, transcap),
-        Cost_Trans_Capacity = convert(Array{Union{Missing,Float64}}, transcapcost)
+        End_Trans_Capacity = zeros(L), # Final availability
+        New_Trans_Capacity = zeros(L), # Expanded capacity
+        Cost_Trans_Capacity = zeros(L) # Expansion Cost
     )
+    # Transmission network reinforcements
+    dfTransCap.End_Trans_Capacity .+= value.(EP[:eAvail_Trans_Cap])
+    if !isempty(EXPANSION_LINES)
+        dfTransCap.New_Trans_Capacity[EXPANSION_LINES] .+= value.(EP[:vNEW_TRANS_CAP][EXPANSION_LINES]).data
+        dfTransCap.Cost_Trans_Capacity[EXPANSION_LINES] .+= dfTransCap.New_Trans_Capacity[EXPANSION_LINES] .* inputs["pC_Line_Reinforcement"][EXPANSION_LINES]
+    end
+    if setup["ParameterScale"] == 1
+        dfTransCap.End_Trans_Capacity *= ModelScalingFactor
+        dfTransCap.New_Trans_Capacity *= ModelScalingFactor
+        dfTransCap.Cost_Trans_Capacity *= ModelScalingFactor^2
+    end
+
 	CSV.write(joinpath(path, "network_expansion.csv"), dfTransCap)
 end
