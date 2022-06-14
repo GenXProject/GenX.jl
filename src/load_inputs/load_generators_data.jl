791cfad72048c2b39de81483cbcdeb440de74ded
Qingyu Xu <qingyux@princeton.edu>
2022-06-14 13:06:27 -0400

    Reserves
    Add a condition statement so that Reg_Cost and Rsv_Cost don't have to exist if no reserve is modeled (i.e., "Reserves" is 0).

--- src/load_inputs/load_generators_data.jl
+++ src/load_inputs/load_generators_data.jl
@@ -163,10 +163,12 @@ function load_generators_data(setup::Dict, path::AbstractString, inputs_gen::Dic
 		# Variable operations and maintenance cost of the charging aspect of a storage technology with STOR = 2,
 		# or variable operations and maintenance costs associated with flexible demand with FLEX = 1
 		inputs_gen["dfGen"][!,:Var_OM_Cost_per_MWh_In] = gen_in[!,:Var_OM_Cost_per_MWh_In]/ModelScalingFactor # Convert to $ million/GWh with objective function in millions
-		# Cost of providing regulation reserves
-		inputs_gen["dfGen"][!,:Reg_Cost] = gen_in[!,:Reg_Cost]/ModelScalingFactor # Convert to $ million/GW with objective function in millions
-		# Cost of providing spinning reserves
-		inputs_gen["dfGen"][!,:Rsv_Cost] = gen_in[!,:Rsv_Cost]/ModelScalingFactor # Convert to $ million/GW with objective function in millions
+		if setup["Reserves"] >= 1
+			# Cost of providing regulation reserves
+			inputs_gen["dfGen"][!,:Reg_Cost] = gen_in[!,:Reg_Cost]/ModelScalingFactor # Convert to $ million/GW with objective function in millions
+			# Cost of providing spinning reserves
+			inputs_gen["dfGen"][!,:Rsv_Cost] = gen_in[!,:Rsv_Cost]/ModelScalingFactor # Convert to $ million/GW with objective function in millions
+		end
 
 		if setup["MultiStage"] == 1
 			inputs_gen["dfGen"][!,:Min_Retired_Cap_MW] = gen_in[!,:Min_Retired_Cap_MW]/ModelScalingFactor
