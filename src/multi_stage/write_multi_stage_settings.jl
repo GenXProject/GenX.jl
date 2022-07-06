25bce19d7d722ca172c2d6079b2edaf767bcac70
jfmorris <jfmorris@mit.edu>
2022-07-06 11:43:07 -0400

    Rename write_settings to write_multi_stage_settings
    To be consistent with other files and to differentiate multi-stage write files from single-stage write files.

--- src/multi_stage/write_multi_stage_settings.jl
+++ src/multi_stage/write_multi_stage_settings.jl
@@ -15,11 +15,11 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 @doc raw"""
-    write_settings(outpath::AbstractString, settings_d::Dict)
+    write_multi_stage_settings(outpath::AbstractString, settings_d::Dict)
 
 Function for writing the multi-stage settings file to the output path for future reference.
 """
-function write_settings(outpath::AbstractString, settings_d::Dict)
+function write_multi_stage_settings(outpath::AbstractString, settings_d::Dict)
     multi_stage_settings_d = settings_d["MultiStageSettingsDict"]
     YAML.write_file(joinpath(outpath, "genx_settings.yml"), settings_d)
     YAML.write_file(joinpath(outpath, "multi_stage_settings.yml"), multi_stage_settings_d)
