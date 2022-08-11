d338498803d70374384d5ac6b38a6c144e062796
Jacob Schwartz <jacob@jaschwartz.net>
2022-08-11 19:38:35 -0400

    Move content of Run*.jl scripts into a function
    This greatly simplifies maintenance of run scripts

--- Example_Systems/RetrofitExample/RetrofitExample_MultiStage/Run.jl
+++ Example_Systems/RetrofitExample/RetrofitExample_MultiStage/Run.jl
@@ -14,8 +14,6 @@ in LICENSE.txt.  Users uncompressing this from an archive may not have
 received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-using Pkg
-include(joinpath(dirname(@__FILE__), "julenv.jl")) #Run this line only for the first time; comment it out for all subsequent use
-println("Activating the Julia virtual environment")
-#Pkg.activate("GenXJulEnv")
-Pkg.status() # Store the path of the current working directory
+using GenX
+
+run_genx_case!(dirname(@__FILE__))
---
+++ src/case_runners/case_runner.jl
@@ -0,0 +1,181 @@
+"""
+GenX: An Configurable Capacity Expansion Model
+Copyright (C) 2021,  Massachusetts Institute of Technology
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+A complete copy of the GNU General Public License v2 (GPLv2) is available
+in LICENSE.txt.  Users uncompressing this from an archive may not have
+received this license file.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+function get_settings_path(case::AbstractString)
+    return joinpath(case, "Settings")
+end
+
+function get_settings_path(case::AbstractString, filename::AbstractString)
+    return joinpath(get_settings_path(case), filename)
+end
+
+function get_default_output_folder(case::AbstractString)
+    return joinpath(case, "Results")
+end
+
+@doc raw"""Run the GenX in the given folder
+case - folder for the case
+"""
+function run_genx_case!(case::AbstractString)
+    genx_settings = get_settings_path(case, "genx_settings.yml") #Settings YAML file path
+    mysetup = configure_settings(genx_settings) # mysetup dictionary stores settings and GenX-specific parameters
+
+    if mysetup["MultiStage"] == 0
+        run_genx_case_simple!(case, mysetup)
+    else
+        run_genx_case_multistage!(case, mysetup)
+    end
+end
+
+function time_domain_reduced_files_exist(tdrpath)
+    tdr_load = isfile(joinpath(tdrpath,"Load_data.csv"))
+    tdr_genvar = isfile(joinpath(tdrpath,"Generators_variability.csv"))
+    tdr_fuels = isfile(joinpath(tdrpath,"Fuels_data.csv"))
+    return (tdr_load && tdr_genvar && tdr_fuels)
+end
+
+function run_genx_case_simple!(case::AbstractString, mysetup::Dict)
+    inputs_path = case
+    settings_path = get_settings_path(case)
+
+    ### Cluster time series inputs if necessary and if specified by the user
+    TDRpath = joinpath(case, mysetup["TimeDomainReductionFolder"])
+
+    if mysetup["TimeDomainReduction"] == 1
+        if !time_domain_reduced_files_exist(TDRpath)
+            println("Clustering Time Series Data (Grouped)...")
+            cluster_inputs(inputs_path, settings_path, mysetup)
+        else
+            println("Time Series Data Already Clustered.")
+        end
+    end
+
+    ### Configure solver
+    println("Configuring Solver")
+    OPTIMIZER = configure_solver(mysetup["Solver"], settings_path)
+
+    #### Running a case
+
+    ### Load inputs
+    println("Loading Inputs")
+    myinputs = load_inputs(mysetup, case)
+
+    println("Generating the Optimization Model")
+    EP = generate_model(mysetup, myinputs, OPTIMIZER)
+
+    println("Solving Model")
+    EP, solve_time = solve_model(EP, mysetup)
+    myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
+
+    # Run MGA if the MGA flag is set to 1 else only save the least cost solution
+    println("Writing Output")
+    outputs_path = get_default_output_folder(case)
+    elapsed_time = @elapsed write_outputs(EP, outputs_path, mysetup, myinputs)
+    println("Time elapsed for writing is")
+    println(elapsed_time)
+    if mysetup["ModelingToGenerateAlternatives"] == 1
+        println("Starting Model to Generate Alternatives (MGA) Iterations")
+        mga(EP, case, mysetup, myinputs, outputs_path)
+    end
+
+    if mysetup["MethodofMorris"] == 1
+        println("Starting Global sensitivity analysis with Method of Morris")
+        morris(EP, inputs_path, mysetup, myinputs, outputs_path, OPTIMIZER)
+    end
+end
+
+
+function run_genx_case_multistage!(case::AbstractString, mysetup::Dict)
+    settings_path = get_settings_path(case)
+    multistage_settings = get_settings_path(case, "multi_stage_settings.yml") # Multi stage settings YAML file path
+    mysetup["MultiStageSettingsDict"] = YAML.load(open(multistage_settings))
+
+    ### Cluster time series inputs if necessary and if specified by the user
+    tdr_settings = get_settings_path(case, "time_domain_reduction_settings.yml") # Multi stage settings YAML file path
+    TDRSettingsDict = YAML.load(open(tdr_settings))
+    TDRpath = joinpath(case, "Inputs", "Inputs_p1", mysetup["TimeDomainReductionFolder"])
+    if mysetup["TimeDomainReduction"] == 1
+        if !time_domain_reduced_files_exist(TDRpath)
+            if (mysetup["MultiStage"] == 1) && (TDRSettingsDict["MultiStageConcatenate"] == 0)
+                println("Clustering Time Series Data (Individually)...")
+                for stage_id in 1:mysetup["MultiStageSettingsDict"]["NumStages"]
+                    cluster_inputs(case, settings_path, mysetup, stage_id)
+                end
+            else
+                println("Clustering Time Series Data (Grouped)...")
+                cluster_inputs(case, settings_path, mysetup)
+            end
+        else
+            println("Time Series Data Already Clustered.")
+        end
+    end
+
+    ### Configure solver
+    println("Configuring Solver")
+    OPTIMIZER = configure_solver(mysetup["Solver"], settings_path)
+
+    model_dict=Dict()
+    inputs_dict=Dict()
+
+    for t in 1:mysetup["MultiStageSettingsDict"]["NumStages"]
+
+        # Step 0) Set Model Year
+        mysetup["MultiStageSettingsDict"]["CurStage"] = t
+
+        # Step 1) Load Inputs
+        inpath_sub = joinpath(case, "Inputs", string("Inputs_p",t))
+
+        inputs_dict[t] = load_inputs(mysetup, inpath_sub)
+        inputs_dict[t] = configure_multi_stage_inputs(inputs_dict[t],mysetup["MultiStageSettingsDict"],mysetup["NetworkExpansion"])
+
+        # Step 2) Generate model
+        model_dict[t] = generate_model(mysetup, inputs_dict[t], OPTIMIZER)
+    end
+
+
+    ### Solve model
+    println("Solving Model")
+
+    # Step 3) Run DDP Algorithm
+    ## Solve Model
+    model_dict, mystats_d, inputs_dict = run_ddp(model_dict, mysetup, inputs_dict)
+
+    # Step 4) Write final outputs from each stage
+
+    outpath = get_default_output_folder(case)
+
+    if !haskey(mysetup, "OverwriteResults") || mysetup["OverwriteResults"] == 1
+        # Overwrite existing results if dir exists
+        # This is the default behaviour when there is no flag, to avoid breaking existing code
+        if !(isdir(outpath))
+            mkdir(outpath)
+        end
+    else
+        # Find closest unused ouput directory name and create it
+        outpath = choose_output_dir(outpath)
+        mkdir(outpath)
+    end
+
+    for p in 1:mysetup["MultiStageSettingsDict"]["NumStages"]
+        outpath_cur = joinpath(outpath, "Results_p$p")
+        write_outputs(model_dict[p], outpath_cur, mysetup, inputs_dict[p])
+    end
+
+    # Step 5) Write DDP summary outputs
+
+    write_multi_stage_outputs(mystats_d, outpath, mysetup, inputs_dict)
+end
+
