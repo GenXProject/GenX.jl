ddbf195638078f90a46864c691fb504b796ae0a8
Jacob Schwartz <jacob@jaschwartz.net>
2022-11-08 17:10:17 -0500

    Feature: allow not listing non-varying resources
    Resources which would have had a column of all 1's in
    generators_variability.csv no longer need to be listed.
    
    Note that even if there is a system without any variable
    resources, a file with the Time_Index column will still be
    required.

--- src/load_inputs/load_generators_variability.jl
+++ src/load_inputs/load_generators_variability.jl
@@ -15,8 +15,18 @@ function load_generators_variability!(setup::Dict, path::AbstractString, inputs:
     filename = "Generators_variability.csv"
     gen_var = load_dataframe(joinpath(my_dir, filename))
 
+    all_resources = inputs["RESOURCES"]
+
+    existing_variability = names(gen_var)
+    for r in all_resources
+        if r ∉ existing_variability
+            @info "assuming availability of 1.0 for resource $r."
+            ensure_column!(gen_var, r, 1.0)
+        end
+    end
+
 	# Reorder DataFrame to R_ID order (order provided in Generators_data.csv)
-	select!(gen_var, [:Time_Index; Symbol.(inputs["RESOURCES"]) ])
+	select!(gen_var, [:Time_Index; Symbol.(all_resources) ])
 
 	# Maximum power output and variability of each energy resource
 	inputs["pP_Max"] = transpose(Matrix{Float64}(gen_var[1:inputs["T"],2:(inputs["G"]+1)]))
