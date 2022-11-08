630c001e79b92fb20ded4f7695f5e0122cf9e5a5
sambuddhac <sambuddha.chakrabarti@gmail.com>
2022-11-08 13:50:49 -0500

    Gotten rid of unnecesary commented lines for reading CSV in the multi_stage write functions

--- src/multi_stage/write_multi_stage_network_expansion.jl
+++ src/multi_stage/write_multi_stage_network_expansion.jl
@@ -31,7 +31,6 @@ function write_multi_stage_network_expansion(outpath::String, settings_d::Dict)
 
     for p in 1:num_stages
         inpath = joinpath(outpath, "Results_p$p")
-        #trans_capacities_d[p] = DataFrame(CSV.File(joinpath(inpath, "network_expansion.csv"), header=true), copycols=true)
         trans_capacities_d[p] = load_dataframe(joinpath(inpath, "network_expansion.csv"))
     end
 
