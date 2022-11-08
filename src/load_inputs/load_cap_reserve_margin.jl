a1aafc89c41d59ae20d0804afe0969e90a747399
Jacob Schwartz <jacob@jaschwartz.net>
2022-11-08 15:02:05 -0500

    Add function for extracting matrix from a df
    This allows sets of columns to be extracted from a dataframe into a matrix, for
    columns which are in any order. Previously the columns needed to be
    consecutive and in order.
    
    For example, the ESR_1, ESR_2, ... ESR_N columns in
    Energy_share_requirement.csv can now be provided in any order.

--- src/load_inputs/load_cap_reserve_margin.jl
+++ src/load_inputs/load_cap_reserve_margin.jl
@@ -7,15 +7,9 @@ function load_cap_reserve_margin!(setup::Dict, path::AbstractString, inputs::Dic
     filename = "Capacity_reserve_margin.csv"
     df = load_dataframe(joinpath(path, filename))
 
-    # Identifying # of planning reserve margin constraints for the system
-    columns = names(df)
-    f = s -> startswith(s, "CapRes")
-    res = count(f, columns)
-    first_col = findfirst(f, columns)
-    last_col = findlast(f, columns)
-
-    inputs["dfCapRes"] = Matrix{Float64}(df[:,first_col:last_col])
-    inputs["NCapacityReserveMargin"] = res
+    mat = extract_matrix_from_dataframe(df, "CapRes")
+    inputs["dfCapRes"] = mat
+    inputs["NCapacityReserveMargin"] = size(mat, 2)
 
     println(filename * " Successfully Read!")
 end
@@ -26,14 +20,9 @@ end
 Read input parameters related to participation of transmission imports/exports in capacity reserve margin constraint.
 """
 function load_cap_reserve_margin_trans!(setup::Dict, inputs::Dict, network_var::DataFrame)
-    columns = names(network_var)
-    f = s -> startswith(s, "DerateCapRes")
-    my_range = findfirst(f, columns):findlast(f, columns)
-    dfDerateTransCapRes = network_var[:, my_range]
-    inputs["dfDerateTransCapRes"] = Matrix{Float64}(dfDerateTransCapRes[completecases(dfDerateTransCapRes),:])
+    mat = extract_matrix_from_dataframe(network_var, "DerateCapRes")
+    inputs["dfDerateTransCapRes"] = mat
 
-    f = s -> startswith(s, "CapRes_Excl")
-    my_range = findfirst(f, columns):findlast(f, columns)
-    dfTransCapRes_excl = network_var[:, my_range]
-    inputs["dfTransCapRes_excl"] = Matrix{Float64}(dfTransCapRes_excl[completecases(dfTransCapRes_excl),:])
+    mat = extract_matrix_from_dataframe(network_var, "CapRes_Excl")
+    inputs["dfTransCapRes_excl"] = mat
 end
