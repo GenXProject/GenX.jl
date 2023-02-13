664931262538fd2624bda6fd4641dae97230cfc8
Jacob Schwartz <jacob@jaschwartz.net>
2023-02-13 17:20:21 -0500

    Refactor the solver configuration
    * less repetition of names, etc
    
    New feature: now *any* key-value pairs can be passed to the solver
    (as long as the keys are strings). Previously, only explicitly specified
    pairs could be passed.
    
    If we really want to prevent the user from passing arbitrary settings
    (say, because JuMP doesn't support them) then we should add a filter or
    other validation, rather than have them silently do nothing.
    
    * fixed a few of the default values listed in the docstrings to match
      the implementation.

--- src/configure_solver/configure_clp.jl
+++ src/configure_solver/configure_clp.jl
@@ -23,47 +23,35 @@ The Clp optimizer instance is configured with the following default parameters i
 function configure_clp(solver_settings_path::String)
 
 	solver_settings = YAML.load(open(solver_settings_path))
+	solver_settings = convert(Dict{String, Any}, solver_settings)
 
-	# Optional solver parameters ############################################
-        MyDualObjectiveLimit = 1e100
-            if(haskey(solver_settings, "DualObjectiveLimit")) MyDualObjectiveLimit = solver_settings["DualObjectiveLimit"] end
-        MyPrimalTolerance = 1e-7	#Primal feasibility tolerance
-            if(haskey(solver_settings, "Feasib_Tol")) MyPrimalTolerance = solver_settings["Feasib_Tol"] end
-        MyDualTolerance = 1e-7	#Dual feasibility tolerance
-            if(haskey(solver_settings, "Feasib_Tol ")) MyDualTolerance = solver_settings["Feasib_Tol"] end
-        MyDualObjectiveLimit = 1e308	#When using dual simplex (where the objective is monotonically changing), terminate when the objective exceeds this limit
-            if(haskey(solver_settings, "DualObjectiveLimit")) MyDualObjectiveLimit = solver_settings["DualObjectiveLimit"] end
-        MyMaximumIterations = 2147483647	#Terminate after performing this number of simplex iterations
-            if(haskey(solver_settings, "MaximumIterations")) MyMaximumIterations = solver_settings["MaximumIterations"] end
-        MyMaximumSeconds = -1.0	#Terminate after this many seconds have passed. A negative value means no time limit
-            if(haskey(solver_settings, "TimeLimit")) MyMaximumSeconds = solver_settings["TimeLimit"] end
-        MyLogLevel = 1	#Set to 1, 2, 3, or 4 for increasing output. Set to 0 to disable output
-            if(haskey(solver_settings, "LogLevel")) MyLogLevel = solver_settings["LogLevel"] end
-        MyPresolveType = 0	#Set to 1 to disable presolve
-            if(haskey(solver_settings, "Pre_Solve")) MyPresolveType = solver_settings["Pre_Solve"] end
-        MySolveType = 5	#Solution method: dual simplex (0), primal simplex (1), sprint (2), barrier with crossover (3), barrier without crossover (4), automatic (5)
-            if(haskey(solver_settings, "Method")) MySolveType = solver_settings["Method"] end
-        MyInfeasibleReturn = 0	#Set to 1 to return as soon as the problem is found to be infeasible (by default, an infeasibility proof is computed as well)
-            if(haskey(solver_settings, "InfeasibleReturn")) MyInfeasibleReturn = solver_settings["InfeasibleReturn"] end
-        MyScaling = 3	#0 -off, 1 equilibrium, 2 geometric, 3 auto, 4 dynamic(later)
-            if(haskey(solver_settings, "Scaling")) MyScaling = solver_settings["Scaling"] end
-        MyPerturbation = 100	#switch on perturbation (50), automatic (100), don't try perturbing (102)
-            if(haskey(solver_settings, "Perturbation")) MyPerturbation = solver_settings["Perturbation"] end
-	########################################################################
+    default_settings = Dict{String,Any}(
+        "Feasib_Tol" => 1e-7,
+        "DualObjectiveLimit" => 1e308,
+        "MaximumIterations" => 2147483647,
+        "TimeLimit" => -1.0,
+        "LogLevel" => 1,
+        "Pre_Solve" => 0,
+        "Method" => 5,
+        "InfeasibleReturn" => 0,
+        "Scaling" => 3,
+        "Perturbation" => 100,
+    )
 
-	OPTIMIZER = optimizer_with_attributes(Clp.Optimizer,
-        "PrimalTolerance" => MyPrimalTolerance,
-        "DualTolerance" => MyDualTolerance,
-        "DualObjectiveLimit" => MyDualObjectiveLimit,
-        "MaximumIterations" => MyMaximumIterations,
-        "MaximumSeconds" => MyMaximumSeconds,
-        "LogLevel" => MyLogLevel,
-        "PresolveType" => MyPresolveType,
-        "SolveType" => MySolveType,
-        "InfeasibleReturn" => MyInfeasibleReturn,
-        "Scaling" => MyScaling,
-        "Perturbation" => MyPerturbation
-	)
+    attributes = merge(default_settings, solver_settings)
 
-	return OPTIMIZER
+    key_replacement = Dict("Feasib_Tol" => "PrimalTolerance",
+                           "TimeLimit" => "MaximumSeconds",
+                           "Pre_Solve" => "PresolveType",
+                           "Method" => "SolveType",
+                          )
+
+    attributes = rename_keys(attributes, key_replacement)
+
+    # TODO this is done to preserve the behavior that Feasib_Tol was used to set both Primal and
+    # Dual tolerances. It should probably be fixed.
+    attributes["DualTolerance"] = attributes["PrimalTolerance"]
+
+    attributes::Dict{String, Any}
+	return optimizer_with_attributes(Clp.Optimizer, attributes...)
 end
