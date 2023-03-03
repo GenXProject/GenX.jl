53f0e4866af2eeddf21f6dba549ea6c5466ec049
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2023-03-03 15:13:11 -0500

    Modify the ramp down of hydro_res

--- src/model/resources/hydro/hydro_res.jl
+++ src/model/resources/hydro/hydro_res.jl
@@ -123,8 +123,7 @@ function hydro_res!(EP::Model, inputs::Dict, setup::Dict)
 
 		# Maximum ramp up and down
 		cRampUp[y in HYDRO_RES, t in 1:T], EP[:vP][y,t]+ EP[:vREG][y,t]+EP[:vRSV][y,t] - EP[:vP][y, hoursbefore(p,t,1)]-EP[:vREG][y, hoursbefore(p,t,1)] <= dfGen[y,:Ramp_Up_Percentage]*EP[:eTotalCap][y]
-		cRampDown[y in HYDRO_RES, t in 1:T], EP[:vP][y, hoursbefore(p,t,1)]+EP[:vREG][y, hoursbefore(p,t,1)]+EP[:vRSV][y, hoursbefore(p,t,1)] - EP[:vP][y,t]-EP[:vREG][y,t]-EP[:vRSV][y,t] <= dfGen[y,:Ramp_Dn_Percentage]*EP[:eTotalCap][y]
-
+		cRampDown[y in HYDRO_RES, t in 1:T], EP[:vP][y, hoursbefore(p,t,1)]+EP[:vREG][y, hoursbefore(p,t,1)] - EP[:vP][y,t] - EP[:vREG][y,t] - EP[:vRSV][y,t] <= dfGen[y,:Ramp_Dn_Percentage]*EP[:eTotalCap][y]
 		# Minimum streamflow running requirements (power generation and spills must be >= min value) in all hours
 		cHydroMinFlow[y in HYDRO_RES, t in 1:T], EP[:vP][y,t] + EP[:vSPILL][y,t] >= dfGen[y,:Min_Power]*EP[:eTotalCap][y]
 		# DEV NOTE: When creating new hydro inputs, should rename Min_Power with Min_flow or similar for clarity since this includes spilled water as well
