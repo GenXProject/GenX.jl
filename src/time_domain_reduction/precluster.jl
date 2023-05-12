26302dd7415b51d7187c234e5a9672e6f034414e
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2023-05-12 00:57:03 -0400

    Update precluster.jl

--- src/time_domain_reduction/precluster.jl
+++ src/time_domain_reduction/precluster.jl
@@ -9,9 +9,7 @@ This function overwrites the time-domain-reduced inputs if they already exist.
 """
 function run_timedomainreduction!(case::AbstractString; stage_id=-99, verbose=false)
     settings_path = get_settings_path(case) #Settings YAML file path
-    genx_settings = get_settings_path(case, "genx_settings.yml") #Settings YAML file path
-    mysetup = configure_settings(genx_settings) # mysetup dictionary stores settings and GenX-specific parameters
-
+    mysetup = configure_settings(case) # mysetup dictionary stores settings and GenX-specific parameters
     cluster_inputs(case, settings_path, mysetup, stage_id, verbose)
     return
 end
