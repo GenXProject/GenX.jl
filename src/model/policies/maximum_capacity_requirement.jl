161d18db25e874ed6e88dedf0e105376946c9dc8
Filippo Pecci <fp0820@princeton.edu>
2023-05-25 15:17:11 -0400

    Fixed issue #461 and bug in policy constraint slacks (#464)
    This fixes an issue where constraints on @expressions for policies were defined before the expressions were fully-developed (with all terms added). This caused those terms to be ignored in the constraints.
    Moving the constraint definitions to lines after the expressions are developed fixes the issue.
    
    Separately, the Max_Cap_Requirement slack variables had an incorrect sign.

--- src/model/policies/maximum_capacity_requirement.jl
+++ src/model/policies/maximum_capacity_requirement.jl
@@ -13,16 +13,17 @@ function maximum_capacity_requirement!(EP::Model, inputs::Dict, setup::Dict)
 	println("Maximum Capacity Requirement Module")
 	NumberOfMaxCapReqs = inputs["NumberOfMaxCapReqs"]
 
-	@constraint(EP, cZoneMaxCapReq[maxcap = 1:NumberOfMaxCapReqs], EP[:eMaxCapRes][maxcap] <= inputs["MaxCapReq"][maxcap])
-
 	# if input files are present, add maximum capacity requirement slack variables
 	if haskey(inputs, "MaxCapPriceCap")
 		@variable(EP, vMaxCap_slack[maxcap = 1:NumberOfMaxCapReqs]>=0)
-		EP[:eMaxCapRes] += vMaxCap_slack
+		EP[:eMaxCapRes] -= vMaxCap_slack
 
 		@expression(EP, eCMaxCap_slack[maxcap = 1:NumberOfMaxCapReqs], inputs["MaxCapPriceCap"][maxcap] * EP[:vMaxCap_slack][maxcap])
 		@expression(EP, eTotalCMaxCapSlack, sum(EP[:eCMaxCap_slack][maxcap] for maxcap = 1:NumberOfMaxCapReqs))
 		
 		EP[:eObj] += eTotalCMaxCapSlack
 	end
+	
+	@constraint(EP, cZoneMaxCapReq[maxcap = 1:NumberOfMaxCapReqs], EP[:eMaxCapRes][maxcap] <= inputs["MaxCapReq"][maxcap])
+
 end
