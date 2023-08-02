gmantegna/GenX

Add ability for storage to contribute to capacity reserves
add outputting of virtual discharge

fix bug

src/model/resources/storage/storage_all.jl
@@ -9,6 +9,7 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 
 	dfGen = inputs["dfGen"]
 	Reserves = setup["Reserves"]
+	capacity_reserve_margin = setup["CapacityReserveMargin"]
 
 	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
 	T = inputs["T"]     # Number of time steps (hours)
@@ -30,6 +31,15 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 
 	# Energy withdrawn from grid by resource "y" at hour "t" [MWh] on zone "z"
 	@variable(EP, vCHARGE[y in STOR_ALL, t=1:T] >= 0);
+
+	# Virtual discharge contributing to capacity reserves at timestep t for storage cluster y
+	@variable(EP, vCAPCONTRSTOR_VP[y in STOR_ALL, t=1:T] >= 0)
+
+	# Virtual charge contributing to capacity reserves at timestep t for storage cluster y
+	@variable(EP, vCAPCONTRSTOR_VCHARGE[y in STOR_ALL, t=1:T] >= 0)
+
+	# Total state of charge being held in reserve at timestep t for storage cluster y
+	@variable(EP, vCAPCONTRSTOR_VS[y in STOR_ALL, t=1:T] >= 0)
 
 	### Expressions ###
 
@@ -89,14 +99,39 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 		# Maximum discharging rate must be less than power rating OR available stored energy in the prior period, whichever is less
 		# wrapping from end of sample period to start of sample period for energy capacity constraint
 		@constraints(EP, begin
-			[y in STOR_ALL, t=1:T], EP[:vP][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_ALL, t=1:T], EP[:vP][y,t] <= EP[:vS][y, hoursbefore(hours_per_subperiod,t,1)]*dfGen[y,:Eff_Down]
+			[y in STOR_ALL, t=1:T], EP[:vP][y,t] + EP[:vCAPCONTRSTOR_VP][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_ALL, t=1:T], EP[:vP][y,t] + EP[:vCAPCONTRSTOR_VP][y,t] <= EP[:vS][y, hoursbefore(hours_per_subperiod,t,1)]*dfGen[y,:Eff_Down]
 		end)
 	end
 	#From co2 Policy module
 	@expression(EP, eELOSSByZone[z=1:Z],
 		sum(EP[:eELOSS][y] for y in intersect(STOR_ALL, dfGen[dfGen[!,:Zone].==z,:R_ID]))
 	)
+
+	# Capacity Reserve Margin policy
+	if capacity_reserve_margin == 1
+		# Constraints governing energy held in reserve when storage makes virtual capacity reserve margin contributions:
+
+		# Links energy held in reserve in first time step with decisions in last time step of each subperiod
+		# We use a modified formulation of this constraint (cVSoCBalLongDurationStorageStart) when operations wrapping and long duration storage are being modeled
+		@constraint(EP, cVSoCBalStart[t in START_SUBPERIODS, y in CONSTRAINTSET], EP[:vCAPCONTRSTOR_VS][y,t] ==
+			EP[:vCAPCONTRSTOR_VS][y,t+hours_per_subperiod-1] + (1/dfGen[y,:Eff_Down] * EP[:vCAPCONTRSTOR_VP][y,t])
+			- (dfGen[y,:Eff_Up]*EP[:vCAPCONTRSTOR_VCHARGE][y,t]) - (dfGen[y,:Self_Disch] * EP[:vCAPCONTRSTOR_VS][y,t+hours_per_subperiod-1]))
+
+		# energy held in reserve for the next hour
+		@constraint(EP, cVSoCBalInterior[t in INTERIOR_SUBPERIODS, y in STOR_ALL], EP[:vCAPCONTRSTOR_VS][y,t] ==
+			EP[:vCAPCONTRSTOR_VS][y,t-1]+(1/dfGen[y,:Eff_Down]*EP[:vCAPCONTRSTOR_VP][y,t])-(dfGen[y,:Eff_Up]*EP[:vCAPCONTRSTOR_VCHARGE][y,t])-(dfGen[y,:Self_Disch]*EP[:vCAPCONTRSTOR_VS][y,t-1]))
+		
+		# energy held in reserve acts as a lower bound on the total energy held in storage
+		@constraint(EP, cSOCMinCapRes[t in 1:T, y in STOR_ALL], EP[:vS][y,t] >= EP[:vCAPCONTRSTOR_VS][y,t])
+	else
+		# Set values for all capacity reserve margin variables to 0
+		@constraints(EP, begin
+			[y in STOR_ALL, t in 1:T], EP[:vCAPCONTRSTOR_VS][y,t] == 0
+			[y in STOR_ALL, t in 1:T], EP[:vCAPCONTRSTOR_VP][y,t] == 0
+			[y in STOR_ALL, t in 1:T], EP[:vCAPCONTRSTOR_VCHARGE][y,t] == 0
+		end)
+	end
 end
 
 function storage_all_reserves!(EP::Model, inputs::Dict)

src/write_outputs/capacity_reserve_margin/write_capacity_value.jl
@@ -41,7 +41,7 @@ function write_capacity_value(path::AbstractString, inputs::Dict, setup::Dict, E
 		temp_capvalue[MUST_RUN_EX, :] = temp_cap_derate[MUST_RUN_EX, :] .* (inputs["pP_Max"][MUST_RUN_EX, :]) .* temp_riskyhour[MUST_RUN_EX, :]
 		temp_capvalue[HYDRO_RES_EX, :] = temp_cap_derate[HYDRO_RES_EX, :] .* (value.(EP[:vP][HYDRO_RES_EX, :])) .* temp_riskyhour[HYDRO_RES_EX, :] ./ totalcap[HYDRO_RES_EX, :]
 		if !isempty(STOR_ALL_EX)
-			temp_capvalue[STOR_ALL_EX, :] = temp_cap_derate[STOR_ALL_EX, :] .* ((value.(EP[:vP][STOR_ALL_EX, :]) - value.(EP[:vCHARGE][STOR_ALL_EX, :]).data)) .* temp_riskyhour[STOR_ALL_EX, :] ./ totalcap[STOR_ALL_EX, :]
+			temp_capvalue[STOR_ALL_EX, :] = temp_cap_derate[STOR_ALL_EX, :] .* ((value.(EP[:vP][STOR_ALL_EX, :]) - value.(EP[:vCHARGE][STOR_ALL_EX, :]).data  + value.(EP[:vCAPCONTRSTOR_VP][STOR_ALL_EX, :]).data - value.(EP[:vCAPCONTRSTOR_VCHARGE][STOR_ALL_EX, :]).data)) .* temp_riskyhour[STOR_ALL_EX, :] ./ totalcap[STOR_ALL_EX, :]
 		end
 		if !isempty(FLEX_EX)
 			temp_capvalue[FLEX_EX, :] = temp_cap_derate[FLEX_EX, :] .* ((value.(EP[:vCHARGE_FLEX][FLEX_EX, :]).data - value.(EP[:vP][FLEX_EX, :]))) .* temp_riskyhour[FLEX_EX, :] ./ totalcap[FLEX_EX, :]

src/write_outputs/capacity_reserve_margin/write_reserve_margin_revenue.jl
@@ -28,7 +28,7 @@ function write_reserve_margin_revenue(path::AbstractString, inputs::Dict, setup:
 		tempresrev[MUST_RUN] = dfGen[MUST_RUN, sym] .* (value.(EP[:eTotalCap][MUST_RUN])) .* (inputs["pP_Max"][MUST_RUN, :] * (dual.(EP[:cCapacityResMargin][i, :])))
 		tempresrev[HYDRO_RES] = dfGen[HYDRO_RES, sym] .* (value.(EP[:vP][HYDRO_RES, :]) * (dual.(EP[:cCapacityResMargin][i, :])))
 		if !isempty(STOR_ALL)
-			tempresrev[STOR_ALL] = dfGen[STOR_ALL, sym] .* ((value.(EP[:vP][STOR_ALL, :]) - value.(EP[:vCHARGE][STOR_ALL, :]).data) * (dual.(EP[:cCapacityResMargin][i, :])))
+			tempresrev[STOR_ALL] = dfGen[STOR_ALL, sym] .* ((value.(EP[:vP][STOR_ALL, :]) - value.(EP[:vCHARGE][STOR_ALL, :]).data + value.(EP[:vCAPCONTRSTOR_VP][STOR_ALL, :]).data - value.(EP[:vCAPCONTRSTOR_VCHARGE][STOR_ALL, :]).data) * (dual.(EP[:cCapacityResMargin][i, :])))
 		end
 		if !isempty(FLEX)
 			tempresrev[FLEX] = dfGen[FLEX, sym] .* ((value.(EP[:vCHARGE_FLEX][FLEX, :]).data - value.(EP[:vP][FLEX, :])) * (dual.(EP[:cCapacityResMargin][i, :])))

src/write_outputs/write_virtual_discharge.jl
@@ -1,0 +1,32 @@
+@doc raw"""
+	write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
+
+Function for writing the "virtual" discharge of each storage technology. Virtual discharge is used to
+	allow storage resources to contribute to the capacity reserve margin without actually discharging.
+"""
+function write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
+	dfGen = inputs["dfGen"]
+	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
+	T = inputs["T"]     # Number of time steps (hours)
+	STOR_ALL = inputs["STOR_ALL"]
+
+	dfVirtualDischarge = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!,:Zone], AnnualSum = Array{Union{Missing,Float64}}(undef, G))
+	virtual_discharge = zeros(G,T)
+
+	scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
+	if !isempty(STOR_ALL)
+	    virtual_discharge[STOR_ALL, :] = (value.(EP[:vCAPCONTRSTOR_VP][STOR_ALL, :]).data - value.(EP[:vCAPCONTRSTOR_VCHARGE][STOR_ALL, :]).data) * scale_factor
+	end
+
+	dfVirtualDischarge.AnnualSum .= virtual_discharge * inputs["omega"]
+	dfVirtualDischarge = hcat(dfVirtualDischarge, DataFrame(virtual_discharge, :auto))
+	auxNew_Names=[Symbol("Resource");Symbol("Zone");Symbol("AnnualSum");[Symbol("t$t") for t in 1:T]]
+	rename!(dfVirtualDischarge,auxNew_Names)
+	total = DataFrame(["Total" 0 sum(dfVirtualDischarge[!,:AnnualSum]) fill(0.0, (1,T))], :auto)
+
+	total[:, 4:T+3] .= sum(virtual_discharge, dims = 1)
+	rename!(total,auxNew_Names)
+	dfVirtualDischarge = vcat(dfVirtualDischarge, total)
+	CSV.write(joinpath(path, "virtual_discharge.csv"), dftranspose(dfVirtualDischarge, false), writeheader=false)
+	return dfVirtualDischarge
+end

GenXProject/GenX.jl

src/model/resources/storage/storage_all.jl
@@ -9,6 +9,7 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 
 	dfGen = inputs["dfGen"]
 	Reserves = setup["Reserves"]
+	capacity_reserve_margin = setup["CapacityReserveMargin"]
 
 	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
 	T = inputs["T"]     # Number of time steps (hours)
@@ -30,6 +31,15 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 
 	# Energy withdrawn from grid by resource "y" at hour "t" [MWh] on zone "z"
 	@variable(EP, vCHARGE[y in STOR_ALL, t=1:T] >= 0);
+
+	# Virtual discharge contributing to capacity reserves at timestep t for storage cluster y
+	@variable(EP, vCAPRES_discharge[y in STOR_ALL, t=1:T] >= 0)
+
+	# Virtual charge contributing to capacity reserves at timestep t for storage cluster y
+	@variable(EP, vCAPRES_charge[y in STOR_ALL, t=1:T] >= 0)
+
+	# Total state of charge being held in reserve at timestep t for storage cluster y
+	@variable(EP, vCAPRES_socinreserve[y in STOR_ALL, t=1:T] >= 0)
 
 	### Expressions ###
 
@@ -89,14 +99,39 @@ function storage_all!(EP::Model, inputs::Dict, setup::Dict)
 		# Maximum discharging rate must be less than power rating OR available stored energy in the prior period, whichever is less
 		# wrapping from end of sample period to start of sample period for energy capacity constraint
 		@constraints(EP, begin
-			[y in STOR_ALL, t=1:T], EP[:vP][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_ALL, t=1:T], EP[:vP][y,t] <= EP[:vS][y, hoursbefore(hours_per_subperiod,t,1)]*dfGen[y,:Eff_Down]
+			[y in STOR_ALL, t=1:T], EP[:vP][y,t] + EP[:vCAPRES_discharge][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_ALL, t=1:T], EP[:vP][y,t] + EP[:vCAPRES_discharge][y,t] <= EP[:vS][y, hoursbefore(hours_per_subperiod,t,1)]*dfGen[y,:Eff_Down]
 		end)
 	end
 	#From co2 Policy module
 	@expression(EP, eELOSSByZone[z=1:Z],
 		sum(EP[:eELOSS][y] for y in intersect(STOR_ALL, dfGen[dfGen[!,:Zone].==z,:R_ID]))
 	)
+
+	# Capacity Reserve Margin policy
+	if capacity_reserve_margin == 1
+		# Constraints governing energy held in reserve when storage makes virtual capacity reserve margin contributions:
+
+		# Links energy held in reserve in first time step with decisions in last time step of each subperiod
+		# We use a modified formulation of this constraint (cVSoCBalLongDurationStorageStart) when operations wrapping and long duration storage are being modeled
+		@constraint(EP, cVSoCBalStart[t in START_SUBPERIODS, y in CONSTRAINTSET], EP[:vCAPRES_socinreserve][y,t] ==
+			EP[:vCAPRES_socinreserve][y,t+hours_per_subperiod-1] + (1/dfGen[y,:Eff_Down] * EP[:vCAPRES_discharge][y,t])
+			- (dfGen[y,:Eff_Up]*EP[:vCAPRES_charge][y,t]) - (dfGen[y,:Self_Disch] * EP[:vCAPRES_socinreserve][y,t+hours_per_subperiod-1]))
+
+		# energy held in reserve for the next hour
+		@constraint(EP, cVSoCBalInterior[t in INTERIOR_SUBPERIODS, y in STOR_ALL], EP[:vCAPRES_socinreserve][y,t] ==
+			EP[:vCAPRES_socinreserve][y,t-1]+(1/dfGen[y,:Eff_Down]*EP[:vCAPRES_discharge][y,t])-(dfGen[y,:Eff_Up]*EP[:vCAPRES_charge][y,t])-(dfGen[y,:Self_Disch]*EP[:vCAPRES_socinreserve][y,t-1]))
+		
+		# energy held in reserve acts as a lower bound on the total energy held in storage
+		@constraint(EP, cSOCMinCapRes[t in 1:T, y in STOR_ALL], EP[:vS][y,t] >= EP[:vCAPRES_socinreserve][y,t])
+	else
+		# Set values for all capacity reserve margin variables to 0
+		@constraints(EP, begin
+			[y in STOR_ALL, t in 1:T], EP[:vCAPRES_socinreserve][y,t] == 0
+			[y in STOR_ALL, t in 1:T], EP[:vCAPRES_discharge][y,t] == 0
+			[y in STOR_ALL, t in 1:T], EP[:vCAPRES_charge][y,t] == 0
+		end)
+	end
 end
 
 function storage_all_reserves!(EP::Model, inputs::Dict)
@@ -146,8 +181,8 @@ function storage_all_reserves!(EP::Model, inputs::Dict)
 
 			# Maximum discharging rate and contribution to reserves up must be less than power rating OR available stored energy in prior period, whichever is less
 			# wrapping from end of sample period to start of sample period for energy capacity constraint
-			[y in STOR_REG_RSV, t=1:T], EP[:vP][y,t]+EP[:vREG_discharge][y,t]+EP[:vRSV_discharge][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_REG_RSV, t=1:T], (EP[:vP][y,t]+EP[:vREG_discharge][y,t]+EP[:vRSV_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
+			[y in STOR_REG_RSV, t=1:T], EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t]+EP[:vREG_discharge][y,t]+EP[:vRSV_discharge][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_REG_RSV, t=1:T], (EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t]+EP[:vREG_discharge][y,t]+EP[:vRSV_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
 		end)
 	end
 	if !isempty(STOR_REG_ONLY)
@@ -174,8 +209,8 @@ function storage_all_reserves!(EP::Model, inputs::Dict)
 
 			# Maximum discharging rate and contribution to reserves up must be less than power rating OR available stored energy in prior period, whichever is less
 			# wrapping from end of sample period to start of sample period for energy capacity constraint
-			[y in STOR_REG_ONLY, t=1:T], EP[:vP][y,t] + EP[:vREG_discharge][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_REG_ONLY, t=1:T], (EP[:vP][y,t]+EP[:vREG_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
+			[y in STOR_REG_ONLY, t=1:T], EP[:vP][y,t] + EP[:vCAPRES_discharge][y,t] + EP[:vREG_discharge][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_REG_ONLY, t=1:T], (EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t]+EP[:vREG_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
 		end)
 	end
 	if !isempty(STOR_RSV_ONLY)
@@ -196,16 +231,16 @@ function storage_all_reserves!(EP::Model, inputs::Dict)
 
 			# Maximum discharging rate and contribution to reserves up must be less than power rating OR available stored energy in prior period, whichever is less
 			# wrapping from end of sample period to start of sample period for energy capacity constraint
-			[y in STOR_RSV_ONLY, t=1:T], EP[:vP][y,t]+EP[:vRSV_discharge][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_RSV_ONLY, t=1:T], (EP[:vP][y,t]+EP[:vRSV_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
+			[y in STOR_RSV_ONLY, t=1:T], EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t]+EP[:vRSV_discharge][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_RSV_ONLY, t=1:T], (EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t]+EP[:vRSV_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
 		end)
 	end
 	if !isempty(STOR_NO_RES)
 		# Maximum discharging rate must be less than power rating OR available stored energy in prior period, whichever is less
 		# wrapping from end of sample period to start of sample period for energy capacity constraint
 		@constraints(EP, begin
-			[y in STOR_NO_RES, t=1:T], EP[:vP][y,t] <= EP[:eTotalCap][y]
-			[y in STOR_NO_RES, t=1:T], EP[:vP][y,t]/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
+			[y in STOR_NO_RES, t=1:T], EP[:vP][y,t]  + EP[:vCAPRES_discharge][y,t] <= EP[:eTotalCap][y]
+			[y in STOR_NO_RES, t=1:T], (EP[:vP][y,t]+EP[:vCAPRES_discharge][y,t])/dfGen[y,:Eff_Down] <= EP[:vS][y, hoursbefore(p,t,1)]
 		end)
 	end
 end

src/write_outputs/capacity_reserve_margin/write_capacity_value.jl
@@ -41,7 +41,7 @@ function write_capacity_value(path::AbstractString, inputs::Dict, setup::Dict, E
 		temp_capvalue[MUST_RUN_EX, :] = temp_cap_derate[MUST_RUN_EX, :] .* (inputs["pP_Max"][MUST_RUN_EX, :]) .* temp_riskyhour[MUST_RUN_EX, :]
 		temp_capvalue[HYDRO_RES_EX, :] = temp_cap_derate[HYDRO_RES_EX, :] .* (value.(EP[:vP][HYDRO_RES_EX, :])) .* temp_riskyhour[HYDRO_RES_EX, :] ./ totalcap[HYDRO_RES_EX, :]
 		if !isempty(STOR_ALL_EX)
-			temp_capvalue[STOR_ALL_EX, :] = temp_cap_derate[STOR_ALL_EX, :] .* ((value.(EP[:vP][STOR_ALL_EX, :]) - value.(EP[:vCHARGE][STOR_ALL_EX, :]).data)) .* temp_riskyhour[STOR_ALL_EX, :] ./ totalcap[STOR_ALL_EX, :]
+			temp_capvalue[STOR_ALL_EX, :] = temp_cap_derate[STOR_ALL_EX, :] .* ((value.(EP[:vP][STOR_ALL_EX, :]) - value.(EP[:vCHARGE][STOR_ALL_EX, :]).data  + value.(EP[:vCAPRES_discharge][STOR_ALL_EX, :]).data - value.(EP[:vCAPRES_charge][STOR_ALL_EX, :]).data)) .* temp_riskyhour[STOR_ALL_EX, :] ./ totalcap[STOR_ALL_EX, :]
 		end
 		if !isempty(FLEX_EX)
 			temp_capvalue[FLEX_EX, :] = temp_cap_derate[FLEX_EX, :] .* ((value.(EP[:vCHARGE_FLEX][FLEX_EX, :]).data - value.(EP[:vP][FLEX_EX, :]))) .* temp_riskyhour[FLEX_EX, :] ./ totalcap[FLEX_EX, :]

src/write_outputs/capacity_reserve_margin/write_reserve_margin_revenue.jl
@@ -28,7 +28,7 @@ function write_reserve_margin_revenue(path::AbstractString, inputs::Dict, setup:
 		tempresrev[MUST_RUN] = dfGen[MUST_RUN, sym] .* (value.(EP[:eTotalCap][MUST_RUN])) .* (inputs["pP_Max"][MUST_RUN, :] * (dual.(EP[:cCapacityResMargin][i, :])))
 		tempresrev[HYDRO_RES] = dfGen[HYDRO_RES, sym] .* (value.(EP[:vP][HYDRO_RES, :]) * (dual.(EP[:cCapacityResMargin][i, :])))
 		if !isempty(STOR_ALL)
-			tempresrev[STOR_ALL] = dfGen[STOR_ALL, sym] .* ((value.(EP[:vP][STOR_ALL, :]) - value.(EP[:vCHARGE][STOR_ALL, :]).data) * (dual.(EP[:cCapacityResMargin][i, :])))
+			tempresrev[STOR_ALL] = dfGen[STOR_ALL, sym] .* ((value.(EP[:vP][STOR_ALL, :]) - value.(EP[:vCHARGE][STOR_ALL, :]).data + value.(EP[:vCAPRES_discharge][STOR_ALL, :]).data - value.(EP[:vCAPRES_charge][STOR_ALL, :]).data) * (dual.(EP[:cCapacityResMargin][i, :])))
 		end
 		if !isempty(FLEX)
 			tempresrev[FLEX] = dfGen[FLEX, sym] .* ((value.(EP[:vCHARGE_FLEX][FLEX, :]).data - value.(EP[:vP][FLEX, :])) * (dual.(EP[:cCapacityResMargin][i, :])))

src/write_outputs/capacity_reserve_margin/write_virtual_discharge.jl
@@ -1,0 +1,32 @@
+@doc raw"""
+	write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
+
+Function for writing the "virtual" discharge of each storage technology. Virtual discharge is used to
+	allow storage resources to contribute to the capacity reserve margin without actually discharging.
+"""
+function write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
+	dfGen = inputs["dfGen"]
+	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
+	T = inputs["T"]     # Number of time steps (hours)
+	STOR_ALL = inputs["STOR_ALL"]
+
+	dfVirtualDischarge = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!,:Zone], AnnualSum = Array{Union{Missing,Float64}}(undef, G))
+	virtual_discharge = zeros(G,T)
+
+	scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
+	if !isempty(STOR_ALL)
+	    virtual_discharge[STOR_ALL, :] = (value.(EP[:vCAPRES_discharge][STOR_ALL, :]).data - value.(EP[:vCAPRES_charge][STOR_ALL, :]).data) * scale_factor
+	end
+
+	dfVirtualDischarge.AnnualSum .= virtual_discharge * inputs["omega"]
+	dfVirtualDischarge = hcat(dfVirtualDischarge, DataFrame(virtual_discharge, :auto))
+	auxNew_Names=[Symbol("Resource");Symbol("Zone");Symbol("AnnualSum");[Symbol("t$t") for t in 1:T]]
+	rename!(dfVirtualDischarge,auxNew_Names)
+	total = DataFrame(["Total" 0 sum(dfVirtualDischarge[!,:AnnualSum]) fill(0.0, (1,T))], :auto)
+
+	total[:, 4:T+3] .= sum(virtual_discharge, dims = 1)
+	rename!(total,auxNew_Names)
+	dfVirtualDischarge = vcat(dfVirtualDischarge, total)
+	CSV.write(joinpath(path, "virtual_discharge.csv"), dftranspose(dfVirtualDischarge, false), writeheader=false)
+	return dfVirtualDischarge
+end