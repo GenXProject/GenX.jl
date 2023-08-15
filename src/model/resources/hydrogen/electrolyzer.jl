e2f01d6296706b3853b623652776ac0e80a717ba
Jesse Jenkins <jesse.d.jenkins@gmail.com>
2023-08-15 17:19:50 -0400

    Completed documentation
    Documentation is now completed and ready for review.

--- src/model/resources/hydrogen/electrolyzer.jl
+++ src/model/resources/hydrogen/electrolyzer.jl
@@ -1,20 +1,27 @@
 @doc raw"""
 	electrolyzer!(EP::Model, inputs::Dict, setup::Dict)
-This function defines the constraints for operation of hydrogen electrolyzers ($y \in EL$) .
 
-	**Ramping limits**
+This function defines the expressions and constraints for operation of hydrogen electrolyzers ($y \in \mathcal{EL} \subset \mathcal{G}$).
+
+**Expressions**
+
+Consumption of electricity by electrolyzer $y$ in time $t$, denoted by $\Theta_{y,z}$, is subtracted from power balance expression `ePowerBalance` (as per other demands or battery charging) and added to Energy Resource Standard policy balance (if applicable), `eESR`.
+
+Revenue from hydrogen production by each electrolyzer $y$, equal to $\omega_t \times \Theta_{y,t} / \eta^{hydrogen}_y \times \$^{hydrogen}_y$, is subtracted from the objective function, where $\eta^{hydrogen}_y$ is the efficiency of the electrolyzer $y$ in megawatt-hours (MWh) of electricity per metric tonne of hydrogen produced and $\$^{hydrogen}_y$ is the price of hydrogen per metric tonne for electrolyzer $y$.
+
+**Ramping limits**
 
 Electrolyzers adhere to the following ramping limits on hourly changes in power output:
 
 ```math
 \begin{aligned}
-	\Theta_{y,z,t-1} - \Theta_{y,z,t} \leq \kappa_{y,z}^{down} \Delta^{\text{total}}_{y,z} \hspace{1cm} \forall y \in \mathcal{EL}, \forall z \in \mathcal{Z}, \forall t \in \mathcal{T}
+	\Theta_{y,t-1} - \Theta_{y,t} \leq \kappa_{y}^{down} \Delta^{\text{total}}_{y}, \hspace{1cm} \forall y \in \mathcal{EL}, \forall t \in \mathcal{T}
 \end{aligned}
 ```
 
 ```math
 \begin{aligned}
-	\Theta_{y,z,t} - \Theta_{y,z,t-1} \leq \kappa_{y,z}^{up} \Delta^{\text{total}}_{y,z} \hspace{1cm} \forall y \in \mathcal{EL}, \forall z \in \mathcal{Z}, \forall t \in \mathcal{T}
+	\Theta_{y,t} - \Theta_{y,t-1} \leq \kappa_{y}^{up} \Delta^{\text{total}}_{y} \hspace{1cm} \forall y \in \mathcal{EL}, \forall t \in \mathcal{T}
 \end{aligned}
 ```
 (See Constraints 1-2 in the code)
@@ -27,18 +34,46 @@ Electrolyzers are bound by the following limits on maximum and minimum power out
 
 ```math
 \begin{aligned}
-	\Theta_{y,z,t} \geq \rho^{min}_{y,z} \times \Delta^{total}_{y,z}
-	\hspace{1cm} \forall y \in \mathcal{EL}, \forall z \in \mathcal{Z}, \forall t \in \mathcal{T}
+	\Theta_{y,t} \geq \rho^{min}_{y} \times \Delta^{total}_{y}
+	\hspace{1cm} \forall y \in \mathcal{EL}, \forall t \in \mathcal{T}
 \end{aligned}
 ```
 
 ```math
 \begin{aligned}
-	\Theta_{y,z,t} \leq \rho^{max}_{y,z,t} \times \Delta^{total}_{y,z}
-	\hspace{1cm} \forall y \in \mathcal{EL}, \forall z \in \mathcal{Z}, \forall t \in \mathcal{T}
+	\Theta_{y,t} \leq \rho^{max}_{y,t} \times \Delta^{total}_{y}
+	\hspace{1cm} \forall y \in \mathcal{EL}, \forall t \in \mathcal{T}
 \end{aligned}
 ```
 (See Constraints 3-4 in the code)
+
+**Minimum annual hydrogen production**
+
+The sum of annual hydrogen production by each electrolyzer $y \in \mathcal{EL}$ must exceed a minimum quantity specified in inputs:
+
+```math
+\begin{aligned}
+	\sum_{t \in T} (\omega_{t} \times \Theta_{y,t} / \eta^{hydrogen}_y) \geq \mathcal{Min kt}_y \times 10^3 
+	\hspace{1cm} \forall y \in \mathcal{EL}
+\end{aligned}
+```
+
+where $\eta^{hydrogen}_y$ is the efficiency of the electrolyzer $y$ in megawatt-hours (MWh) of electricity per metric tonne of hydrogen produced and $\mathcal{Min kt}_y$ is the minimum annual quantity of hydrogen that must be produced by electrolyzer $y$ in kilotonnes. 
+(See constraint 5 in the code)
+
+**Hourly clean supply matching constraint**
+
+This constraint requires generation from qualified resources ($y \in \mathcal{Qualified}$, indicated by `Qualified_Hydrogen_Supply==1` in `Generators_data.csv`) from within the same zone $z$ as the electrolyzers are located to be >= hourly consumption from electrolyzers in the zone (and any charging by qualified storage within the zone used to help increase electrolyzer utilization):
+
+```math
+\begin{aligned}
+	\sum_{y \in \{z \cap \mathcal{Qualified}\}} \Theta_{y,t} \geq \sum_{y \in \{z \cap \mathcal{EL}\}} \Theta_{y,t} + \sum_{y \in \{z \cap \mathcal{Qualified} \cap \mathcal{STOR}\}}  \Pi_{y,t}
+	\hspace{1cm} \forall z \in \mathcal{Z}, \forall t \in \mathcal{T}, 
+\end{aligned}
+```
+(See constraint 6 in the code)
+
+This constraint permits modeling of the 'three pillars' requirements for clean hydrogen supply of (1) new clean supply (if only new clean resources are designated as eligible), (2) that is deliverable to the electrolyzer (assuming co-location within the same modeled zone = deliverability), and (3) produced within the same hour as the electrolyzer consumes power (otherwise known as 'additionality/new supply', 'deliverability', and 'temporal matching requirements') See Ricks, Xu & Jenkins (2023), ''Minimizing emissions from grid-based hydrogen production in the United States'' *Environ. Res. Lett.* 18 014025 [doi:10.1088/1748-9326/acacb5](https://iopscience.iop.org/article/10.1088/1748-9326/acacb5/meta) for more. 
 """
 
 function electrolyzer!(EP::Model, inputs::Dict, setup::Dict)
@@ -102,7 +137,7 @@ function electrolyzer!(EP::Model, inputs::Dict, setup::Dict)
 
 	end
 
-	### Minimum hydrogen production constraint (if any)
+	### Minimum hydrogen production constraint (if any) (Constraint #5)
 	@constraint(EP, 
 		cHydrogenMin[y in ELECTROLYZERS], sum(inputs["omega"][t] * EP[:vUSE][y,t] / dfGen[y,:Hydrogen_MWh_Per_Tonne] for t=1:T) >= dfGen[y,:Electrolyzer_Min_kt]*10^3
 	)
@@ -112,10 +147,10 @@ function electrolyzer!(EP::Model, inputs::Dict, setup::Dict)
 		[y in ELECTROLYZERS, t in 1:T], EP[:vP][y,t] == 0
 	end)
 
-	### Hourly Hydrogen Matching Constraint ###
+	### Hourly Hydrogen Matching Constraint (Constraint #6) ### 
 	# Requires generation from qualified resources (indicated by Qualified_Hydrogen_Supply==1 in Generators_data.csv) 
-	# from within the same zone as the electrolyzers are located to be >= hourly consumption from electrolyzers (and any charging by 
-	# qualified storage within the zone used to help increase electrolyzer utilization)
+	# from within the same zone as the electrolyzers are located to be >= hourly consumption from electrolyzers in the zone 
+	# (and any charging by qualified storage within the zone used to help increase electrolyzer utilization)
 	HYDROGEN_ZONES = unique(dfGen.Zone[dfGen.ELECTROLYZER.==1])
 	QUALIFIED_SUPPLY = dfGen.R_ID[dfGen.Qualified_Hydrogen_Supply.==1]
 	@constraint(EP, cHourlyMatching[z in HYDROGEN_ZONES, t=1:T], 
@@ -123,8 +158,8 @@ function electrolyzer!(EP::Model, inputs::Dict, setup::Dict)
 	)
 
 
-	### ESR Policy ###
-	# Since we're using vUSE to denote electrolyzer consumption, we subtract this from the eESR balance to increase demand for clean resources if desired
+	### Energy Resource Standard Policy ###
+	# Since we're using vUSE to denote electrolyzer consumption, we subtract this from the eESR Energy Resource Standard balance to increase demand for clean resources if desired
 	# Electrolyzer demand is only accounted for in an ESR that the electrolyzer resources is tagged in in Generates_data.csv (e.g. ESR_N > 0) and 
 	# a share of electrolyzer demand equal to dfGen[y,:ESR_N] must be met by resources qualifying for ESR_N for each electrolyzer resource y.
 	if setup["EnergyShareRequirement"] >= 1
