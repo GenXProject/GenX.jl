f33915a5735d06acd02e9d10184aaf94f0b43ea7
Ruaridh Macdonald <rmacd@mit.edu>
2023-08-17 19:20:25 -0400

    Simplify expression manipulation functions
    Remove fill_with_zeros on scalar expression
    
    Update expression_manipulation_test.jl
    
    Fix CRM bug with empty expression
    
    createemptyexpression -> create_empty_expression
    
    Simplify types in expression_manipulation.jl
    - We still need to compare the performance and compile time using this approach, but the memory usage and runtime appear the same for now

--- src/model/expression_manipulation.jl
+++ src/model/expression_manipulation.jl
@@ -1,29 +1,27 @@
-function createemptyexpression!(EP::Model, exprname::Symbol, dim1::Int64)
-    temp = Vector{AffExpr}(undef, dim1)
-    for i=1:dim1::Int64
-        temp[i] = AffExpr(0.0)
-    end
-    EP[exprname] = temp
+###### ###### ###### ###### ###### ######
+# Create dense arrays of expressions filled with zeros to be added to later
+###### ###### ###### ###### ###### ######
+
+function create_empty_expression!(EP::Model, exprname::Symbol)
+    EP[exprname] = AffExpr(0.0)
+    return nothing
 end
 
-function createemptyexpression!(EP::Model, exprname::Symbol, dim1::Int64, dim2::Int64)
-    temp = Matrix{AffExpr}(undef, (dim1, dim2))
-    for j=1:dim2
-        for i=1:dim1
-            temp[i,j] = AffExpr(0.0)
-        end
-    end
+function create_empty_expression!(EP::Model, exprname::Symbol, dim1::Int64)
+    temp = Array{AffExpr}(undef, dim1)
+    fill_with_zeros!(temp)
     EP[exprname] = temp
+    return nothing
 end
 
-function createemptyexpression!(EP::Model, exprname::Symbol, dims::NTuple{N, Int64}) where N
+function create_empty_expression!(EP::Model, exprname::Symbol, dims::NTuple{N, Int64}) where N
     temp = Array{AffExpr}(undef, dims)
     fill_with_zeros!(temp)
     EP[exprname] = temp
     return nothing
 end
 
-function createemptyexpression!(EP::Model, exprname::Symbol, dims::Vector{Int64})
+function create_empty_expression!(EP::Model, exprname::Symbol, dims::Vector{Int64})
     # The version using tuples is slightly less memory
     temp = Array{AffExpr}(undef, dims...)
     fill_with_zeros!(temp)
@@ -31,21 +29,35 @@ function createemptyexpression!(EP::Model, exprname::Symbol, dims::Vector{Int64}
     return nothing
 end
 
-function fill_with_zeros!(arr::Array{AffExpr, dims}) where dims
+###### ###### ###### ###### ###### ######
+# Fill dense arrays of expressions with zeros or a constant
+###### ###### ###### ###### ###### ######
+
+function fill_with_zeros!(arr::Array{GenericAffExpr{C,T}, dims}) where {C,T,dims}
     for i::Int64 in eachindex(IndexLinear(), arr)::Base.OneTo{Int64}
         arr[i] = AffExpr(0.0)
     end
     return nothing
 end
 
-function fill_with_const!(arr::Array{AffExpr, dims}, con::Float64) where dims
+function fill_with_const!(arr::Array{GenericAffExpr{C,T}, dims}, con::Real) where {C,T,dims}
     for i::Int64 in eachindex(IndexLinear(), arr)::Base.OneTo{Int64}
         arr[i] = AffExpr(con)
     end
     return nothing
 end
 
-function _add_similar_to_expression!(expr1::Array{C, dim1}, expr2::Array{T, dim2}) where {C,T,dim1,dim2}
+###### ###### ###### ###### ###### ######
+# Element-wise addition of one expression into another
+# Both arrays must have the same dimensions
+###### ###### ###### ###### ###### ######
+
+function add_similar_to_expression!(expr1::GenericAffExpr{C,T}, expr2::V) where {C,T,V}
+    add_to_expression!(expr1, expr2)
+    return nothing
+end
+
+function add_similar_to_expression!(expr1::Array{GenericAffExpr{C,T}, dim1}, expr2::Array{V, dim2}) where {C,T,V,dim1,dim2}
     # This is defined for Arrays of different dimensions
     # despite the fact it will definitely throw an error
     # because the error will tell the user / developer
@@ -57,46 +69,47 @@ function _add_similar_to_expression!(expr1::Array{C, dim1}, expr2::Array{T, dim2
     return nothing
 end
 
-function add_similar_to_expression!(expr1::Array{GenericAffExpr{C,T}, dim1}, expr2::Array{GenericAffExpr{C,T}, dim2}) where {C,T,dim1,dim2}
-    _add_similar_to_expression!(expr1, expr2)
+function add_similar_to_expression!(expr1::AbstractArray{GenericAffExpr{C,T}, dim1}, expr2::AbstractArray{V, dim2}) where {C,T,V,dim1,dim2}
+    # This is defined for Arrays of different dimensions
+    # despite the fact it will definitely throw an error
+    # because the error will tell the user / developer
+    # the dimensions of both arrays
+    check_sizes_match(expr1, expr2)
+    for i in eachindex(expr1)
+        add_to_expression!(expr1[i], expr2[i])
+    end
+    return nothing
 end
 
-function add_similar_to_expression!(expr1::Array{GenericAffExpr{C,T}, dim1}, expr2::Array{GenericVariableRef{C}, dim2}) where {C,T,dim1,dim2}
-    _add_similar_to_expression!(expr1, expr2)
+###### ###### ###### ###### ###### ######
+# Element-wise addition of one term into an expression
+# Both arrays must have the same dimensions
+###### ###### ###### ###### ###### ######
+
+function add_term_to_expression!(expr1::GenericAffExpr{C,T}, expr2::V) where {C,T,V}
+    add_to_expression!(expr1, expr2)
+    return nothing
 end
 
-function add_similar_to_expression!(expr1::Array{GenericAffExpr{C,T}, dim1}, expr2::Array{AbstractJuMPScalar, dim2}) where {C,T,dim1,dim2}
-    _add_similar_to_expression!(expr1, expr2)
-end
-
-function add_similar_to_expression!(expr1::Array{GenericAffExpr{C,T}, dim1}, expr2::Array{Float64, dim2}) where {C,T,dim1,dim2}
-    _add_similar_to_expression!(expr1, expr2)
-end
-
-function _add_term_to_expression!(expr1::Array{C, dims}, expr2::T) where {C,T,dims}
+function add_term_to_expression!(expr1::Array{GenericAffExpr{C,T}, dims}, expr2::V) where {C,T,V,dims}
     for i in eachindex(IndexLinear(), expr1)
         add_to_expression!(expr1[i], expr2)
     end
     return nothing
 end
 
-function add_term_to_expression!(expr1::Array{GenericAffExpr{C,T}, dims}, expr2::Float64) where {C,T,dims}
-    _add_term_to_expression!(expr1, expr2)
+function add_term_to_expression!(expr1::AbstractArray{GenericAffExpr{C,T}, dims}, expr2::V) where {C,T,V,dims}
+    for i in eachindex(expr1)
+        add_to_expression!(expr1[i], expr2)
+    end
+    return nothing
 end
 
-function add_term_to_expression!(expr1::Array{GenericAffExpr{C,T}, dims}, expr2::GenericAffExpr{C,T}) where {C,T,dims}
-    _add_term_to_expression!(expr1, expr2)
-end
+###### ###### ###### ###### ###### ######
+# Check that two arrays have the same dimensions
+###### ###### ###### ###### ###### ######
 
-function add_term_to_expression!(expr1::Array{GenericAffExpr{C,T}, dims}, expr2::GenericVariableRef{C}) where {C,T,dims}
-    _add_term_to_expression!(expr1, expr2)
-end
-
-function add_term_to_expression!(expr1::Array{GenericAffExpr{C,T}, dims}, expr2::AbstractJuMPScalar) where {C,T,dims}
-    _add_term_to_expression!(expr1, expr2)
-end
-
-function check_sizes_match(expr1::Array{C, dim1}, expr2::Array{T, dim2}) where {C,T,dim1, dim2}
+function check_sizes_match(expr1::AbstractArray{C, dim1}, expr2::AbstractArray{T, dim2}) where {C,T,dim1, dim2}
     # After testing, this appears to be just as fast as a method for Array{GenericAffExpr{C,T}, dims} or Array{AffExpr, dims}
     if size(expr1) != size(expr2)
         error("
@@ -105,7 +118,11 @@ function check_sizes_match(expr1::Array{C, dim1}, expr2::Array{T, dim2}) where {
     end
 end
 
-function _sum_expression(expr::AbstractArray{C, dims}) where {C,dims}
+###### ###### ###### ###### ###### ######
+# Sum an array of expressions into a single expression
+###### ###### ###### ###### ###### ######
+
+function sum_expression(expr::AbstractArray{C, dims}) where {C,dims}
     # This appears to work just as well as a separate method for Array{AffExpr, dims}
     total = AffExpr(0.0)
     for i in eachindex(expr)
