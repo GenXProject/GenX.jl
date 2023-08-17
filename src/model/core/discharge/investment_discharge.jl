c577ecec12d19df5f860db9ee64cb5bf84c807e0
Gabe Mantegna <gabe.mantegna@princeton.edu>
2023-08-17 09:56:01 -0700

    Revert "fix non-working instance of add to expression"
    This reverts commit 04cc9a81b7eb67ce729bddd5ccaa76e1e6908f6b.

--- src/model/core/discharge/investment_discharge.jl
+++ src/model/core/discharge/investment_discharge.jl
@@ -169,11 +169,11 @@ function investment_discharge!(EP::Model, inputs::Dict, setup::Dict)
 
 	if setup["MinCapReq"] == 1
 		@expression(EP, eMinCapResInvest[mincap = 1:inputs["NumberOfMinCapReqs"]], sum(EP[:eTotalCap][y] for y in dfGen[dfGen[!, Symbol("MinCapTag_$mincap")] .== 1, :R_ID]))
-		EP[:eMinCapRes] += eMinCapResInvest
+		add_similar_to_expression!(EP[:eMinCapRes], eMinCapResInvest)
 	end
 
 	if setup["MaxCapReq"] == 1
 		@expression(EP, eMaxCapResInvest[maxcap = 1:inputs["NumberOfMaxCapReqs"]], sum(EP[:eTotalCap][y] for y in dfGen[dfGen[!, Symbol("MaxCapTag_$maxcap")] .== 1, :R_ID]))
-		EP[:eMaxCapRes] += eMaxCapResInvest
+		add_similar_to_expression!(EP[:eMaxCapRes], eMaxCapResInvest)
 	end
 end
