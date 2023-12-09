688574ccf6b45fd9ac34844f872bf3c2bb43e2b2
lbonaldo <bonaldo.luca12@gmail.com>
2023-12-08 20:14:32 -0500

    Add resource type and interface Add load_resources_data.jl matching load_generators_data.jl

--- src/load_inputs/load_resources_data.jl
+++ src/load_inputs/load_resources_data.jl
@@ -1,12 +1,314 @@
-function load_resources_data!(setup::Dict, path::AbstractString, inputs_gen::Dict, fuel_costs::Dict, fuel_CO2::Dict)
-    if isfile(joinpath(path, "Generators_data.csv"))
+function _get_resource_info()        
+    resources = (
+        hydro   = (filename="hydro.csv", type=HYDRO), #key="HYDRO_RES"),
+        thermal = (filename="thermal.csv", type=THERM), #key="THERM_ALL"),
+        vre     = (filename="vre.csv", type=VRE), #key="VRE"),
+        storage = (filename="storage.csv", type=STOR), #key="STOR_ALL"),
+        flex_demand  = (filename="flex_demand.csv", type=FLEX), #key="FLEX"),
+        electrolyzer = (filename="electrolyzer.csv", type=ELECTROLYZER), #key="ELECTROLYZER")
+    )
+    return resources
+end
+
+function scale_resources_data!(resource_in::DataFrame, scale_factor::Float64)
+    # See documentation for descriptions of each column
+    # Generally, these scalings converts energy and power units from MW to GW
+    # and $/MW to $M/GW. Both are done by dividing the values by 1000.
+    columns_to_scale = [:existing_charge_cap_mw,        # to GW
+                        :existing_cap_mwh,              # to GWh
+                        :existing_cap_mw,               # to GW
+
+                        :cap_size,                      # to GW
+
+                        :min_cap_mw,                    # to GW
+                        :min_cap_mwh,                   # to GWh
+                        :min_charge_cap_mw,             # to GWh
+
+                        :max_cap_mw,                    # to GW
+                        :max_cap_mwh,                   # to GWh
+                        :max_charge_cap_mw,             # to GW
+
+                        :inv_cost_per_mwyr,             # to $M/GW/yr
+                        :inv_cost_per_mwhyr,            # to $M/GWh/yr
+                        :inv_cost_charge_per_mwyr,      # to $M/GW/yr
+
+                        :fixed_om_cost_per_mwyr,        # to $M/GW/yr
+                        :fixed_om_cost_per_mwhyr,       # to $M/GWh/yr
+                        :fixed_om_cost_charge_per_mwyr, # to $M/GW/yr
+
+                        :var_om_cost_per_mwh,           # to $M/GWh
+                        :var_om_cost_per_mwh_in,        # to $M/GWh
+
+                        :reg_cost,                      # to $M/GW
+                        :rsv_cost,                      # to $M/GW
+
+                        :min_retired_cap_mw,            # to GW
+                        :min_retired_charge_cap_mw,     # to GW
+                        :min_retired_energy_cap_mw,     # to GW
+
+                        :start_cost_per_mw,             # to $M/GW
+
+                        :ccs_disposal_cost_per_metric_ton,
+
+                        :hydrogen_mwh_per_tonne       	# to GWh/t
+                        ]
+
+    for column in columns_to_scale
+        if string(column) in names(resource_in)
+            resource_in[!, column] /= scale_factor
+        end
+    end
+    return nothing
+end
+
+function required_columns_for_co2()
+    return ["Biomass", "CO2_Capture_Fraction", "CO2_Capture_Fraction_Startup", "CCS_Disposal_Cost_per_Metric_Ton"]
+end
+
+function ensure_columns!(df::DataFrame)
+    # write zeros if col names are not in the gen_in dataframe
+    required_cols = [required_columns_for_co2()...,]
+    for col in required_cols
+		ensure_column!(df, col, 0)
+	end
+end
+
+function _get_resources_array(path::AbstractString, Resource, scale_factor::Float64=1.0)
+    # load dataframe with data of a given resource
+    resource_in = load_dataframe(path)
+    # scale data if necessary
+    scale_resources_data!(resource_in, scale_factor)
+    # ensure columns
+    ensure_columns!(resource_in)
+    # convert dataframe to array of resources of correct type
+    resources::Vector{Resource} = Resource.(dataframerow_to_tuple.(eachrow(resource_in)))
+    # return resources
+    return resources
+end
+
+function _get_all_resources(resources_folder::AbstractString, resources_info::NamedTuple, scale_factor::Float64=1.0)
+    # resources_offset = 0
+    resources = Vector(undef, length(resources_info))
+    # loop over available types and get all resources
+    # for (i,(filename, resource_type, key)) in enumerate(values(resources_info))
+    for (i,(filename, resource_type)) in enumerate(values(resources_info))
+        # path to resources data
+        path = joinpath(resources_folder, filename)
+        # load resources data of a given type
+        resources_same_type = _get_resources_array(path, resource_type, scale_factor)
+        # add resources of a given type to array of resources
+        resources[i] = resources_same_type
+        @info filename * " Successfully Read."
+        # get indices of resources for later use
+        # resources_indices = _get_resources_indices(resources_same_type, resources_offset)
+        # add indices to input_data
+        # input_data[key] = resources_indices
+        # update offset
+        # resources_offset += length(resources_same_type)
+    end
+    return reduce(vcat, resources)
+end
+
+function _get_resources_indices(resource::Vector{<:AbstractResource}, offset::Int64)
+    # return array of indices of resources
+    range = (1,length(resource)) .+ offset
+    return UnitRange{Int64}(range...)
+end
+
+function load_resources_data!(setup::Dict, case_path::AbstractString, input_data::Dict)
+    if isfile(joinpath(case_path, "Generators_data.csv"))
         Base.depwarn(
             "The `Generators_data.csv` file will be deprecated in a future release. " *
             "Please use the new interface for generators creation, and see the documentation for additional details.",
             :load_resources_data!, force=true)
-        load_generators_data!(setup, path, inputs_gen, fuel_costs, fuel_CO2)
+        load_generators_data!(setup, case_path, input_data)
+        translate_generators_data!(setup, input_data)
     else
-        # load all the resources data
+        # Scale factor for energy and currency units
+        scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
+
+        # get path to resources data
+        resources_folder = setup["ResourcesPath"]
+        resources_folder = joinpath(case_path,resources_folder)
+
+        resources = load_scaled_resources_data(resources_folder, scale_factor)
+
+        add_resources_to_input_data!(setup, input_data, resources)
+
+        return nothing
     end
+end
+
+function load_scaled_resources_data(resources_folder::AbstractString, scale_factor::Float64=1.0)
+    # get type, filename and resource-key for each type of resources
+    resources_info = _get_resource_info()
+    # load each resource type, scale data and return array of resources
+    resources = _get_all_resources(resources_folder, resources_info, scale_factor)
+    # validate input before returning
+    validate_resources(resources)
+    return resources
+end
+
+function add_resources_to_input_data!(setup::Dict, input_data::Dict, resources::Vector{<:AbstractResource})
+    
+    # Number of resources
+    G = length(resources)
+    input_data["G"] = G
+
+    # Number of time steps (periods)
+    𝑇 = input_data["T"]
+    
+    ## HYDRO
+    # Set of all reservoir hydro resources
+    input_data["HYDRO_RES"] = hydro(resources)
+    # Set of hydro resources modeled with known reservoir energy capacity
+    if !isempty(input_data["HYDRO_RES"])
+        input_data["HYDRO_RES_KNOWN_CAP"] = intersect(input_data["HYDRO_RES"], has_hydro_energy_to_power_ratio(resources))
+    end
+
+    ## STORAGE
+    # Set of storage resources with symmetric charge/discharge capacity
+    input_data["STOR_SYMMETRIC"] = symmetric_storage(resources)
+    # Set of storage resources with asymmetric (separte) charge/discharge capacity components
+    input_data["STOR_ASYMMETRIC"] = asymmetric_storage(resources)
+    # Set of all storage resources
+    input_data["STOR_ALL"] = union(input_data["STOR_SYMMETRIC"],input_data["STOR_ASYMMETRIC"])
+
+    # Set of storage resources with long duration storage capabilitites
+    input_data["STOR_HYDRO_LONG_DURATION"] = intersect(input_data["HYDRO_RES"], is_LDS(resources))
+    input_data["STOR_LONG_DURATION"] = intersect(input_data["STOR_ALL"], is_LDS(resources))	    
+    input_data["STOR_SHORT_DURATION"] = intersect(input_data["STOR_ALL"], is_SDS(resources))
+
+    ## VRE
+    # Set of controllable variable renewable resources
+    input_data["VRE"] = vre(resources)
+
+    ## FLEX
+    # Set of flexible demand-side resources
+    input_data["FLEX"] = flex_demand(resources)
+
+    ## TODO: MUST_RUN
+    # Set of must-run plants - could be behind-the-meter PV, hydro run-of-river, must-run fossil or thermal plants
+    # input_data["MUST_RUN"] = must_run(resources)
+
+    ## ELECTROLYZER
+    # Set of hydrogen electolyzer resources:
+    input_data["ELECTROLYZER"] = electrolyzer(resources)
+
+    ## Retrofit ## TODO: ask how to add it
+
+    ## Reserves
+    if setup["Reserves"] >= 1
+        # Set for resources with regulation reserve requirements
+        input_data["REG"] = has_regulation_reserve_requirements(resources)
+        # Set for resources with spinning reserve requirements
+        input_data["RSV"] = has_spinning_reserve_requirements(resources)
+    end
+
+    ## THERM
+    # Set of all thermal resources
+    input_data["THERM_ALL"] = thermal(resources)
+    # Unit commitment
+    if setup["UCommit"] >= 1
+        # Set of thermal resources with unit commitment
+        input_data["THERM_COMMIT"] = has_unit_commitment(resources)
+        # Set of thermal resources without unit commitment
+        input_data["THERM_NO_COMMIT"] = no_unit_commitment(resources)
+        # Start-up cost is sum of fixed cost per start startup
+		input_data["C_Start"] = zeros(Float64, G, 𝑇)
+        for g in input_data["THERM_COMMIT"]
+            start_up_cost = start_cost_per_mw(resources[g]) * cap_size(resources[g])
+            input_data["C_Start"][g,:] .= start_up_cost
+        end
+        # Piecewise fuel usage option
+        # process_piecewisefuelusage!(input_data, scale_factor)
+    else
+        # Set of thermal resources with unit commitment
+        input_data["THERM_COMMIT"] = []
+        # Set of thermal resources without unit commitment
+        input_data["THERM_NO_COMMIT"] = input_data["THERM_ALL"]
+    end
+    # For now, the only resources eligible for UC are themal resources
+    input_data["COMMIT"] = input_data["THERM_COMMIT"]
+
+    ## Co-located resources
+    # VRE and storage
+    # load_vre_stor_data!(input_data, setup, path)
+
+    buildable = is_buildable(resources)
+    retirable = is_retirable(resources)
+
+    # Set of all resources eligible for new capacity
+    input_data["NEW_CAP"] = intersect(buildable, has_max_capacity_mw(resources))
+    # Set of all resources eligible for capacity retirements
+    input_data["RET_CAP"] = intersect(retirable, has_existing_capacity_mw(resources))
+
+    new_cap_energy = Set{Int64}()
+    ret_cap_energy = Set{Int64}()
+    if !isempty(input_data["STOR_ALL"])
+        # Set of all storage resources eligible for new energy capacity
+        new_cap_energy = intersect(buildable, has_max_capacity_mwh(resources), input_data["STOR_ALL"])
+        # Set of all storage resources eligible for energy capacity retirements
+        ret_cap_energy = intersect(retirable, has_existing_capacity_mwh(resources), input_data["STOR_ALL"])
+    end
+    input_data["NEW_CAP_ENERGY"] = new_cap_energy
+    input_data["RET_CAP_ENERGY"] = ret_cap_energy
+
+	new_cap_charge = Set{Int64}()
+	ret_cap_charge = Set{Int64}()
+	if !isempty(input_data["STOR_ASYMMETRIC"])
+		# Set of asymmetric charge/discharge storage resources eligible for new charge capacity
+        new_cap_charge = intersect(buildable, has_max_charge_capacity_mw(resources), input_data["STOR_ASYMMETRIC"])
+		# Set of asymmetric charge/discharge storage resources eligible for charge capacity retirements
+        ret_cap_charge = intersect(buildable, has_existing_charge_capacity_mw(resources), input_data["STOR_ASYMMETRIC"])
+	end
+	input_data["NEW_CAP_CHARGE"] = new_cap_charge
+	input_data["RET_CAP_CHARGE"] = ret_cap_charge
+
+    # Names of resources
+    input_data["RESOURCES"] = resource_name.(resources)
+
+    # Zones resources are located in
+    zones = zone_id.(resources)
+    
+    # Resource identifiers by zone (just zones in resource order + resource and zone concatenated)
+    input_data["R_ZONES"] = zones
+    input_data["RESOURCE_ZONES"] = input_data["RESOURCES"] .* "_z" .* string.(zones)
+
+    # Fuel
+    input_data["HAS_FUEL"] = has_fuel(resources)
+
+    input_data["resources"] = resources
     return nothing
 end
+
+function translate_generators_data!(setup::Dict, inputs_gen::Dict)
+end
+
+# function extract_matrix_from_resources(resources::Vector{<:AbstractResource}, columnprefix::AbstractString, prefixseparator='_')
+    
+#     for i in eachindex(resources)
+#     end
+
+#     all_columns = string.(keys(resource))
+#     columnnumbers = _find_matrix_columns(all_columns, columnprefix, prefixseparator)
+
+#     if length(columnnumbers) == 0
+#         msg = """an input dataframe with columns $all_columns was searched for
+#         numbered columns starting with $columnprefix, but nothing was found."""
+#         error(msg)
+#     end
+
+#     # check that the sequence of column numbers is 1..N
+#     if columnnumbers != collect(1:length(columnnumbers))
+#         msg = """the columns $columns in an input file must be numbered in
+#         a complete sequence from 1...N. It looks like some of the sequence is missing.
+#         This error could also occur if there are two columns with the same number."""
+#         error(msg)
+#     end
+
+#     sorted_columns = columnprefix .* prefixseparator .* string.(columnnumbers)
+# end
+
+
+
--- src/model/resources/resources.jl
+++ src/model/resources/resources.jl
@@ -1,10 +1,163 @@
-GenXResource = Dict{Symbol, Any}
+abstract type AbstractResource end
+
+const resources_type = (:ELECTROLYZER, :FLEX, :HYDRO, :STOR, :THERM, :VRE)
+for r in resources_type
+    let nt = Symbol("nt"), r = r
+        @eval begin
+            struct $r{names,T} <: AbstractResource
+                $nt::NamedTuple{names,T}
+            end
+            Base.parent(e::$r) = getfield(e, $(QuoteNode(nt)))
+        end
+    end
+end
+
+function Base.getproperty(r::AbstractResource, sym::Symbol)
+    haskey(parent(r), sym) && return getproperty(parent(r), sym)
+    throw(ErrorException("type $(nameof(typeof(r))) has no field $(string(sym))"))
+end
+# Base.getproperty(x::MyStruct,y::Symbol) = Base.getproperty(getfield(x,:nt),y)
+Base.setproperty!(r::AbstractResource, sym::Symbol, value) = throw(ErrorException("setfield!: immutable struct of type $(nameof(typeof(r))) cannot be changed"))
+
+function Base.haskey(r::AbstractResource, sym::Symbol)
+    return haskey(parent(r), sym)
+end
+
+function Base.get(r::AbstractResource, sym::Symbol, default) 
+    return haskey(r, sym) ? getproperty(r,sym) : default
+end
+
+function Base.getproperty(rs::Vector{AbstractResource}, sym::Symbol)
+    # if sym is Type then return all resources of that type
+    sym ∈ resources_type && return rs[isa.(rs, GenX.eval(sym))]
+    # if sym is a field of the resource then return that field for all resources
+    return [getproperty(r, sym) for r in rs]
+end
+
+Base.pairs(r::AbstractResource) = pairs(parent(r))
+
+function Base.show(io::IO, r::AbstractResource) 
+    for (k,v) in pairs(r)
+        println(io, "$k: $v")
+    end
+end
+
+# For backward compatibility
+const GenXResource = Dict{Symbol, Any}
 
 # interface with generators_data.csv
 # acts as a global variable
 resource_attribute_not_set() = 0
 
+# interface for all resources
 resource_name(r::GenXResource) = r[:Resource]
+resource_name(r::AbstractResource)::String = r.resource
+
+zone_id(r::GenXResource) = r[:Zone]
+zone_id(r::AbstractResource) = r.zone
+
+start_cost_per_mw(r::AbstractResource)::Float64 = r.start_cost_per_mw
+
+cap_size(r::AbstractResource)::Float64 = r.cap_size
+
+function is_buildable(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :new_build, resource_attribute_not_set()) == 1, rs)
+end
+
+function is_retirable(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :can_retire, resource_attribute_not_set()) == 1, rs)
+end
+
+# TODO: default values for resource attributes
+function has_max_capacity_mw(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :max_cap_mw, resource_attribute_not_set()) != 0, rs)
+end
+
+
+function has_existing_capacity_mw(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :existing_cap_mw, resource_attribute_not_set()) != 0, rs)
+end
+
+function has_fuel(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :fuel, resource_attribute_not_set()) != "None", rs)
+end
+
+# STOR interface
+storage(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,STOR), rs)
+
+function symmetric_storage(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> isa(r,STOR) && r.type == 1, rs)
+end
+
+function asymmetric_storage(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> isa(r,STOR) && r.type == 2, rs)
+end
+
+function is_LDS(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :LDS, resource_attribute_not_set()) > 0, rs)
+end
+
+function is_SDS(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :LDS, resource_attribute_not_set()) == 0, rs)
+end
+
+function has_max_capacity_mwh(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :max_cap_mwh, resource_attribute_not_set()) != 0, rs)
+end
+
+function has_existing_capacity_mwh(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :existing_cap_mwh, resource_attribute_not_set()) >= 0, rs)
+end
+
+function has_max_charge_capacity_mw(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :max_charge_cap_mw, resource_attribute_not_set()) != 0, rs)
+end
+
+function has_existing_charge_capacity_mw(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :existing_charge_cap_mw, resource_attribute_not_set()) >= 0, rs)
+end
+
+# HYDRO interface
+hydro(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,HYDRO), rs)
+function has_hydro_energy_to_power_ratio(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :hydro_energy_to_power_ratio, resource_attribute_not_set()) > 0, rs)
+end
+
+# Retrofit
+function has_retrofit(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :retro, resource_attribute_not_set()) > 0, rs)
+end
+
+## THERM interface
+thermal(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,THERM), rs)
+# Unit commitment
+function has_unit_commitment(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> isa(r,THERM) && r.type == 1, rs)
+end
+# Without unit commitment
+function no_unit_commitment(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> isa(r,THERM) && r.type == 2, rs)
+end
+
+# Reserves
+function has_regulation_reserve_requirements(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :reg_max, resource_attribute_not_set()) > 0, rs)
+end
+
+function has_spinning_reserve_requirements(rs::Vector{T}) where T <: AbstractResource
+    return findall(r -> get(r, :rsv_max, resource_attribute_not_set()) > 0, rs)
+end
+
+# VRE interface
+vre(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,VRE), rs)
+
+# Electrolyzer interface
+electrolyzer(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,ELECTROLYZER), rs)
+
+# Flex interface
+flex_demand(rs::Vector{T}) where T <: AbstractResource = findall(r -> isa(r,FLEX), rs)
+
+
 
 @doc raw"""
 	check_resource_type_flags(r::GenXResource)
@@ -93,6 +246,20 @@ function check_longdurationstorage_applicability(r::GenXResource)
     return error_strings
 end
 
+function check_LDS_applicability(r::AbstractResource)
+    applicable_resources = Union{STOR, HYDRO}
+    not_set = resource_attribute_not_set()
+    error_strings = String[]
+    lds_value = get(r, :LDS, not_set)
+    # LDS is available onlåy for Hydro and Storage
+    if !isa(r, applicable_resources) && lds_value > 0
+        e = string("Resource ", resource_name(r), " has :LDS = ", lds_value, ".\n",
+                   "This setting is valid only for resources where the type is one of $applicable_resources.")
+        push!(error_strings, e)
+    end
+    return error_strings
+end
+
 @doc raw"""
 	check_maintenance_applicability(r::GenXResource)
 
@@ -130,6 +297,34 @@ function check_maintenance_applicability(r::GenXResource)
     return error_strings
 end
 
+function check_maintenance_applicability(r::AbstractResource)
+    applicable_resources = THERM
+
+    not_set = resource_attribute_not_set()
+    maint_value = get(r, :MAINT, not_set)
+    
+    error_strings = String[]
+    
+    if maint_value == not_set
+        # not MAINT so the rest is not applicable
+        return error_strings
+    end
+
+    # MAINT is available only for Thermal
+    if !isa(r, applicable_resources) && maint_value > 0
+        e = string("Resource ", resource_name(r), " has :MAINT = ", maint_value, ".\n",
+                   "This setting is valid only for resources where the type is one of $applicable_resources.")
+        push!(error_strings, e)
+    end
+    if get(r, :THERM, not_set) == 2
+        e = string("Resource ", resource_name(r), " has :MAINT = ", maint_value, ".\n",
+                   "This is valid only for resources with unit commitment (:THERM = 1);\n",
+                   "this has :THERM = 2.")
+        push!(error_strings, e)
+    end
+    return error_strings
+end
+
 @doc raw"""
     check_resource(r::GenXResource)::Vector{String}
 
@@ -145,13 +340,20 @@ function check_resource(r::GenXResource)::Vector{String}
     return e
 end
 
+function check_resource(r::AbstractResource)::Vector{String}
+    e = String[]
+    e = [e; check_LDS_applicability(r)]
+    e = [e; check_maintenance_applicability(r)]
+    return e
+end
+
 @doc raw"""
     check_resource(resources::Vector{GenXResource})::Vector{String}
 
 Validate the consistency of a vector of GenX resources
 Reports any errors in a list of strings.
 """
-function check_resource(resources::Vector{GenXResource})::Vector{String}
+function check_resource(resources::T)::Vector{String} where T <: Union{Vector{GenXResource}, Vector{AbstractResource}}
     e = String[]
     for r in resources
         e = [e; check_resource(r)]
@@ -168,7 +370,7 @@ function announce_errors_and_halt(e::Vector{String})
     error(s)
 end
 
-function validate_resources(resources::Vector{GenXResource})
+function validate_resources(resources::T) where T <: Union{Vector{GenXResource}, Vector{AbstractResource}}
     e = check_resource(resources)
     if length(e) > 0
         announce_errors_and_halt(e)
@@ -177,4 +379,51 @@ end
 
 function dataframerow_to_dict(dfr::DataFrameRow)
     return Dict(pairs(dfr))
+end
+
+function dataframerow_to_tuple(dfr::DataFrameRow)
+    return NamedTuple(pairs(dfr))
+end
+
+
+## Utility functions for working with resources
+
+function in_zone(resource::GenXResource, zone::Int)::Bool
+    zone_id(resource) == zone
+end
+in_zone(r::AbstractResource, zone::Int)::Bool = r.zone == zone
+
+@doc raw"""
+    resources_in_zone(resources::Vector{GenXResource}, zone::Int)::Vector{GenXResources)
+Find resources in a zone.
+"""
+function resources_in_zone(resources::Vector{GenXResource}, zone::Int)::Vector{GenXResource}
+    return filter(r -> in_zone(r, zone), resources)
+end
+resources_in_zone(rs::Vector{AbstractResource}, zone::Int)::Vector{AbstractResource} = filter(r -> in_zone(r, zone), rs)
+
+@doc raw"""
+    resources_in_zone_by_name(inputs::Dict, zone::Int)::Vector{String}
+Find names of resources in a zone.
+"""
+function resources_in_zone_by_name(inputs::Dict, zone::Int)::Vector{String}
+    resources_d = inputs["resources_d"]
+    return resource_name.(resources_in_zone(resources_d, zone))
+end
+
+@doc raw"""
+    resources_in_zone_by_rid(df::DataFrame, zone::Int)::Vector{Int}
+Find R_ID's of resources in a zone.
+"""
+function resources_in_zone_by_rid(df::DataFrame, zone::Int)::Vector{Int}
+    return df[df.Zone .== zone, :R_ID]
+end
+
+@doc raw"""
+    resources_in_zone_by_rid(inputs::Dict, zone::Int)::Vector{Int}
+Find R_ID's of resources in a zone.
+"""
+function resources_in_zone_by_rid(inputs::Dict, zone::Int)::Vector{Int}
+    df = inputs["dfGen"]
+    return resources_in_zone_by_rid(df, zone)
 end
