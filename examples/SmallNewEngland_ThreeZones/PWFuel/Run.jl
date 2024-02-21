68bf3c4d4eb4c39effa80f2b31af7594c1ffbe7c
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2024-02-21 16:50:03 -0500

    Changed the name of the folder Policies to Policies_Slack, gotten rid of Gurobi optimizer from all Run.jl files

--- examples/SmallNewEngland_ThreeZones/PWFuel/Run.jl
+++ examples/SmallNewEngland_ThreeZones/PWFuel/Run.jl
@@ -1,6 +1,3 @@
 using GenX
-using Gurobi
 
-optim = Gurobi.Optimizer
-
-run_genx_case!(dirname(@__FILE__), optim)
+run_genx_case!(dirname(@__FILE__))
--- examples/SmallNewEngland_ThreeZones/Policies/Run.jl
+++
@@ -1,6 +0,0 @@
-using GenX
-using Gurobi
-
-optim = Gurobi.Optimizer
-
-run_genx_case!(dirname(@__FILE__), optim)
