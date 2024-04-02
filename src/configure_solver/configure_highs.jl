ed24f3a8a6437656740fab7895d24406362957ea
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2024-04-02 15:11:23 -0400

    Update configure_highs.jl
    Gotten rid of further non-essential parameters

--- src/configure_solver/configure_highs.jl
+++ src/configure_solver/configure_highs.jl
@@ -16,15 +16,6 @@ The HiGHS optimizer instance is configured with the following default parameters
 	Pre_Solve: choose          # Presolve option: "off", "choose" or "on" # [type: string, advanced: false, default: "choose"]
 	Method: ipm #choose        #HiGHS-specific solver settings # Solver option: "simplex", "choose" or "ipm" # [type: string, advanced: false, default: "choose"] In order to run a case when the UCommit is set to 1, i.e. MILP instance, set the Method to choose
 	
-	#HiGHS-specific solver settings
-	# Parallel option: "off", "choose" or "on"
-	# [type: string, advanced: false, default: "choose"]
-	parallel: choose
-	
-	# Compute cost, bound, RHS and basic solution ranging: "off" or "on"
-	# [type: string, advanced: false, default: "off"]
-	ranging: off
-	
 	# Limit on cost coefficient: values larger than this will be treated as infinite
 	# [type: double, advanced: false, range: [1e+15, inf], default: 1e+20]
 	infinite_cost: 1e+20
@@ -32,14 +23,6 @@ The HiGHS optimizer instance is configured with the following default parameters
 	# Limit on |constraint bound|: values larger than this will be treated as infinite
 	# [type: double, advanced: false, range: [1e+15, inf], default: 1e+20]
 	infinite_bound: 1e+20
-	
-	# Lower limit on |matrix entries|: values smaller than this will be treated as zero
-	# [type: double, advanced: false, range: [1e-12, inf], default: 1e-09]
-	small_matrix_value: 1e-09
-	
-	# Upper limit on |matrix entries|: values larger than this will be treated as infinite
-	# [type: double, advanced: false, range: [1, inf], default: 1e+15]
-	large_matrix_value: 1e+15
 	
 	# IPM optimality tolerance
 	# [type: double, advanced: false, range: [1e-12, inf], default: 1e-08]
@@ -52,14 +35,6 @@ The HiGHS optimizer instance is configured with the following default parameters
 	# Objective target for termination
 	# [type: double, advanced: false, range: [-inf, inf], default: -inf]
 	objective_target: -Inf
-	
-	# random seed used in HiGHS
-	# [type: HighsInt, advanced: false, range: {0, 2147483647}, default: 0]
-	random_seed: 0
-	
-	# number of threads used by HiGHS (0: automatic)
-	# [type: HighsInt, advanced: false, range: {0, 2147483647}, default: 0]
-	threads: 0
 """
 function configure_highs(solver_settings_path::String, optimizer::Any)
 
@@ -72,17 +47,11 @@ function configure_highs(solver_settings_path::String, optimizer::Any)
         "Pre_Solve" => "choose",
         "TimeLimit" => Inf,
         "Method" => "ipm",
-        "parallel" => "choose",
-        "ranging" => "off",
         "infinite_cost" => 1e+20,
         "infinite_bound" => 1e+20,
-        "small_matrix_value" => 1e-09,
-        "large_matrix_value" => 1e+15,
         "ipm_optimality_tolerance" => 1e-08,
         "objective_bound" => Inf,
         "objective_target" => -Inf,
-        "random_seed" => 0,
-        "threads" => 0,
     )
 
     attributes = merge(default_settings, solver_settings)
