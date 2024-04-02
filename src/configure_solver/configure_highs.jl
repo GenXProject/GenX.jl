825107055f3aff1b1d925adb9dbd908de9d4d922
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2024-04-02 16:14:33 -0400

    Update configure_highs.jl
    Removed infinite bound, infinite cost, objective bound, objective target, small matrix and large matrix values.

--- src/configure_solver/configure_highs.jl
+++ src/configure_solver/configure_highs.jl
@@ -16,25 +16,9 @@ The HiGHS optimizer instance is configured with the following default parameters
 	Pre_Solve: choose          # Presolve option: "off", "choose" or "on" # [type: string, advanced: false, default: "choose"]
 	Method: ipm #choose        #HiGHS-specific solver settings # Solver option: "simplex", "choose" or "ipm" # [type: string, advanced: false, default: "choose"] In order to run a case when the UCommit is set to 1, i.e. MILP instance, set the Method to choose
 	
-	# Limit on cost coefficient: values larger than this will be treated as infinite
-	# [type: double, advanced: false, range: [1e+15, inf], default: 1e+20]
-	infinite_cost: 1e+20
-	
-	# Limit on |constraint bound|: values larger than this will be treated as infinite
-	# [type: double, advanced: false, range: [1e+15, inf], default: 1e+20]
-	infinite_bound: 1e+20
-	
 	# IPM optimality tolerance
 	# [type: double, advanced: false, range: [1e-12, inf], default: 1e-08]
 	ipm_optimality_tolerance: 1e-08
-	
-	# Objective bound for termination
-	# [type: double, advanced: false, range: [-inf, inf], default: inf]
-	objective_bound: Inf
-	
-	# Objective target for termination
-	# [type: double, advanced: false, range: [-inf, inf], default: -inf]
-	objective_target: -Inf
 
 	# Run the crossover routine for IPX
 	# [type: string, advanced: "on", range: {"off", "on"}, default: "off"]
@@ -51,11 +35,7 @@ function configure_highs(solver_settings_path::String, optimizer::Any)
         "Pre_Solve" => "choose",
         "TimeLimit" => Inf,
         "Method" => "ipm",
-        "infinite_cost" => 1e+20,
-        "infinite_bound" => 1e+20,
         "ipm_optimality_tolerance" => 1e-08,
-        "objective_bound" => Inf,
-        "objective_target" => -Inf,
 	"run_crossover" => "off",
     )
 
