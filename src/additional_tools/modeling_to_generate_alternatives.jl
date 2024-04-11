0214204daed68307fd3553e040a69ca7d14718c1
ml6802 <ml6802@princeton.edu>
2024-04-11 14:35:27 -0400

    Added production or capacity optionality

--- src/additional_tools/modeling_to_generate_alternatives.jl
+++ src/additional_tools/modeling_to_generate_alternatives.jl
@@ -68,7 +68,7 @@ function mga(EP::Model, path::AbstractString, setup::Dict, inputs::Dict)
             ### Maximization objective
             @objective(EP,
                 Max,
-                sum(pRand[tt, z] * EP[:vSumvCap][tt, z]
+                sum(pRand[tt, z] * EP[:vMGA][tt, z]
                 for tt in 1:length(TechTypes), z in 1:Z))
 
             # Solve Model Iteration
@@ -83,7 +83,7 @@ function mga(EP::Model, path::AbstractString, setup::Dict, inputs::Dict)
             ### Minimization objective
             @objective(EP,
                 Min,
-                sum(pRand[tt, z] * EP[:vSumvCap][tt, z]
+                sum(pRand[tt, z] * EP[:vMGA][tt, z]
                 for tt in 1:length(TechTypes), z in 1:Z))
 
             # Solve Model Iteration
@@ -119,6 +119,8 @@ function mga!(EP::Model, inputs::Dict)
 
     Z = inputs["Z"]     # Number of zones
     gen = inputs["RESOURCES"]    # Resources data
+    # Read set of MGA variables
+    var_set = setup["MGAVariableSet"] ### Choose setting in genx_settings.yaml: MGAVariableSet: 1 = annual generation, otherwise, sum of capacity
     # Create a set of unique technology types
     resources_with_mga_on = gen[ids_with_mga(gen)]
     TechTypes = unique(resource_type_mga.(resources_with_mga_on))
@@ -130,8 +132,13 @@ function mga!(EP::Model, inputs::Dict)
     end
     # Constraint to compute total generation in each zone from a given Technology Type
     ### Variables ###
-    @variable(EP, vSumvCap[TechTypes = 1:length(TechTypes), z = 1:Z]>=0)
+    @variable(EP, vMGA[TechTypes = 1:length(TechTypes), z = 1:Z]>=0)
 
     ### Constraint ###
-    @constraint(EP, cCapEquiv[tt = 1:length(TechTypes), z = 1:Z], vSumvCap[tt,z] == sum(EP[:eTotalCap][y] for y in resource_in_zone_with_TechType(tt, z)))
+    if var_set==1
+        @constraint(EP, cSumProd[tt=1:length(TechTypes), z = 1:Z], vMGA[tt,z] == sum(EP[:vP][y,t] * inputs["omega"][t]
+	    for y in dfGen[(dfGen[!,:Resource_Type] .== TechTypes[tt]) .& (dfGen[!,:Zone] .== z),:][!,:R_ID], t in 1:T))
+    else
+        @constraint(EP, cCapEquiv[tt = 1:length(TechTypes), z = 1:Z], vMGA[tt,z] == sum(EP[:eTotalCap][y] for y in resource_in_zone_with_TechType(tt, z)))
+    end
 end
