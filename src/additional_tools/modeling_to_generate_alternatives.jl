349d85bccb40954a836949711fb8422297ffc934
lbonaldo <bonaldo.luca12@gmail.com>
2024-04-12 15:24:06 -0400

    Include both constraints to docstring

--- src/additional_tools/modeling_to_generate_alternatives.jl
+++ src/additional_tools/modeling_to_generate_alternatives.jl
@@ -3,20 +3,35 @@
 
 We have implemented an updated Modeling to Generate Alternatives (MGA) Algorithm proposed by [Berntsen and Trutnevyte (2017)](https://www.sciencedirect.com/science/article/pii/S0360544217304097) to generate a set of feasible, near cost-optimal technology portfolios. This algorithm was developed by [Brill Jr, E. D., 1979](https://pubsonline.informs.org/doi/abs/10.1287/mnsc.25.5.413) and introduced to energy system planning by [DeCarolia, J. F., 2011](https://www.sciencedirect.com/science/article/pii/S0140988310000721).
 
-To create the MGA formulation, we replace the cost-minimizing objective function of GenX with a new objective function that creates multiple generation portfolios by zone. We further add a new budget constraint based on the optimal objective function value $f^*$ of the least-cost model and the user-specified value of slack $\delta$. After adding the slack constraint, the resulting MGA formulation is given as:
+To create the MGA formulation, we replace the cost-minimizing objective function of GenX with a new objective function that creates multiple generation portfolios by zone. We further add a new budget constraint based on the optimal objective function value $f^*$ of the least-cost model and the user-specified value of slack $\delta$. After adding the slack constraint, the resulting MGA formulation is given as (`MGAAnnualGeneration = 0` in the genx_settings.yml file, or not set):
 
 ```math
 \begin{aligned}
 	\text{max/min} \quad
 	&\sum_{z \in \mathcal{Z}}\sum_{r \in \mathcal{R}} \beta_{z,r}^{k}P_{z,r}\\
 	\text{s.t.} \quad
-	&P_{z,r} = \sum_{g \in \mathcal{G}}C_{g,z}  \\
+	&P_{z,r} = \sum_{y \in \mathcal{G}}C_{y,z,r}  \\
 	& f \leq f^* + \delta \\
 	&Ax = b
 \end{aligned}
 ```
 
-where, $\beta_{zr}$ is a random objective function coefficient betwen $[0,1]$ for MGA iteration $k$. We aggregate capacity into a new variable $P_{z,r}$ that represents total capacity from technology type $r$ in a zone $z$. In the second constraint above, $\delta$ denote the increase in budget from the least-cost solution and $f$ represents the expression for the total system cost. The constraint $Ax = b$ represents all other constraints in the power system model. We then solve the formulation with minimization and maximization objective function to explore near optimal solution space.
+where, $\beta_{z,r}$ is a random objective function coefficient betwen $[0,1]$ for MGA iteration $k$. We aggregate capacity into a new variable $P_{z,r}$ that represents total capacity from technology type $r$ in a zone $z$. 
+
+If the user set `MGAAnnualGeneration = 1` in the genx_settings.yml file, the MGA formulation is given as:
+```math
+\begin{aligned}
+\text{max/min} \quad
+	&\sum_{z \in \mathcal{Z}}\sum_{r \in \mathcal{R}} \beta_{z,r}^{k}P_{z,r}\\
+	\text{s.t.} \quad
+	&P_{z,r} = \sum_{y \in \mathcal{G}}\sum_{t \in \mathcal{T}} \omega_{t} \Theta_{y,t,z,r}  \\
+	& f \leq f^* + \delta \\
+	&Ax = b
+\end{aligned}
+```
+where, $\beta_{z,r}$ is a random objective function coefficient betwen $[0,1]$ for MGA iteration $k$. $\Theta_{y,t,z,r}$ is a generation of technology $y$ in zone $z$ in time period $t$ that belongs to a resource type $r$. We aggregate $\Theta_{y,t,z,r}$ into a new variable $P_{z,r}$ that represents total generation from technology type $r$ in a zone $z$. 
+
+In the second constraint in both the above formulations, $\delta$ denote the increase in budget from the least-cost solution and $f$ represents the expression for the total system cost. The constraint $Ax = b$ represents all other constraints in the power system model. We then solve the formulation with minimization and maximization objective function to explore near optimal solution space.
 """
 function mga(EP::Model, path::AbstractString, setup::Dict, inputs::Dict)
     if setup["ModelingToGenerateAlternatives"] == 1
@@ -101,11 +116,23 @@ function mga(EP::Model, path::AbstractString, setup::Dict, inputs::Dict)
     end
 end
 
-"""
+@doc raw"""
     mga!(EP::Model, inputs::Dict)
 
 This function reads the input data, collect the resources with MGA flag on and creates a set of unique technology types. 
 The function then adds a constraint to the model to compute total capacity in each zone from a given Technology Type.
+
+If the user set `MGAAnnualGeneration = 0` in the genx_settings.yml file, the constraint has the following form:
+```math
+P_{z,r} = \sum_{y \in \mathcal{G}}C_{y,z,r}
+```
+where, the aggregated capacity $P_{z,r}$ represents total capacity from technology type $r$ in a zone $z$. 
+
+If the user set `MGAAnnualGeneration = 1` in the genx_settings.yml file, the constraint has the following form:
+```math
+P_{z,r} = \sum_{y \in \mathcal{G}}\sum_{t \in \mathcal{T}} \omega_{t} \Theta_{y,t,z,r} 
+```
+where $\Theta_{y,t,z,r}$ is a generation of technology $y$ in zone $z$ in time period $t$ that belongs to a resource type $r$. $\Theta_{y,t,z,r}$ is aggregated into a new variable $P_{z,r}$ that represents total generation from technology type $r$ in a zone $z$. 
 
 # Arguments
 - `EP::Model`: GenX model object
