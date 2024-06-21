8b1871f223628d1f059e02d689b914f8c2ea3616
lbonaldo <bonaldo.luca12@gmail.com>
2024-06-21 16:55:14 -0400

    Add pkgversion function for Julia versions < 1.9

--- src/startup/genx_startup.jl
+++ src/startup/genx_startup.jl
@@ -2,6 +2,7 @@ function __init__()
     print_genx_version()
 end
 
+
 function print_genx_version()
     v = pkgversion(GenX)
     ascii_art = raw"""  ____           __  __   _ _
@@ -14,4 +15,25 @@ function print_genx_version()
     ascii_art *= "Version: $(v)"
     println(ascii_art)
     return nothing
+end
+
+
+# This function is a workaround for Julia versions < 1.9.
+function pkgversion(m::Module)
+    if VERSION >= v"1.9"
+        return Base.pkgversion(m)
+    else
+        _pkgdir = pkgdir(m)
+        _pkgdir === nothing && return nothing
+        project_file = joinpath(_pkgdir, "Project.toml")
+        isa(project_file, String) && return get_project_version(project_file)
+        return nothing
+    end
+end
+
+function get_project_version(project_file::String)
+    d = Base.parsed_toml(project_file)
+    v = get(d, "version", nothing)
+    isnothing(v) && return nothing
+    return VersionNumber(v::AbstractString)
 end
