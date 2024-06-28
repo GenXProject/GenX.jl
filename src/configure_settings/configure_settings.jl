6bf24dfc9f92cba653485628fa2e1a2cd8a65475
lbonaldo <bonaldo.luca12@gmail.com>
2024-06-28 18:51:51 -0400

    Move config multistage to configure settings

--- src/configure_settings/configure_settings.jl
+++ src/configure_settings/configure_settings.jl
@@ -168,3 +168,34 @@ function configure_writeoutput(output_settings_path::String, settings::Dict)
     end
     return writeoutput
 end
+
+function default_settings_multistage()
+    Dict{Any, Any}("NumStages" => 3,
+        "StageLengths" => [10, 10, 10],
+        "WACC" => 0.045,
+        "ConvergenceTolerance" => 0.01,
+        "Myopic" => 1)
+end
+
+@doc raw"""
+    configure_settings(settings_path::String, output_settings_path::String)
+
+Reads in the settings from the `multi_stage_settings.yml` YAML file and
+merges them with the default multistage settings. It then returns the
+settings dictionary.
+
+# Arguments
+- `settings_path::String`: The path to the multistage settings YAML file.
+
+# Returns
+- `settings::Dict`: The multistage settings dictionary.
+"""
+function configure_settings_multistage(settings_path::String)
+    println("Configuring Multistage Settings")
+    model_settings = YAML.load(open(settings_path))
+
+    settings = default_settings_multistage()
+    merge!(settings, model_settings)
+
+    return settings
+end
