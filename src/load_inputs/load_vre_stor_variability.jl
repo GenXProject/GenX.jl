27b414f3afa5663164df52473d86b73272313423
lbonaldo <bonaldo.luca12@gmail.com>
2024-07-24 11:31:52 +0200

    Allow wind and solar in VRE_stor to be loaded separately

--- src/load_inputs/load_vre_stor_variability.jl
+++ src/load_inputs/load_vre_stor_variability.jl
@@ -12,36 +12,48 @@ function load_vre_stor_variability!(setup::Dict, path::AbstractString, inputs::D
     # if TDR is used, my_dir = TDR_directory, else my_dir = "system"
     my_dir = get_systemfiles_path(setup, TDR_directory, path)
 
-    filename1 = "Vre_and_stor_solar_variability.csv"
-    vre_stor_solar = load_dataframe(joinpath(my_dir, filename1))
-
-    filename2 = "Vre_and_stor_wind_variability.csv"
-    vre_stor_wind = load_dataframe(joinpath(my_dir, filename2))
-
+    # Resource names
     all_resources = inputs["RESOURCE_NAMES"]
 
-    function ensure_column_zeros!(vre_stor_df, all_resources)
-        existing_variability = names(vre_stor_df)
-        for r in all_resources
-            if r ∉ existing_variability
-                ensure_column!(vre_stor_df, r, 0.0)
-            end
-        end
+    # SOLAR VARIABILITY
+    if !isempty(inputs["VS_SOLAR"])
+        filename = "Vre_and_stor_solar_variability.csv"
+        filepath = joinpath(my_dir, filename)
+        load_process_variability!(filepath, all_resources, inputs, "Solar")
     end
 
-    ensure_column_zeros!(vre_stor_solar, all_resources)
-    ensure_column_zeros!(vre_stor_wind, all_resources)
+    # WIND VARIABILITY
+    if !isempty(inputs["VS_WIND"])
+        filename = "Vre_and_stor_wind_variability.csv"
+        filepath = joinpath(my_dir, filename)
+        load_process_variability!(filepath, all_resources, inputs, "Wind")
+    end
+
+    return nothing
+end
+
+function load_process_variability!(filepath::AbstractString, all_resources::Vector{T},
+        inputs::Dict, maxpower_key::String) where T <: AbstractString
+    vre_stor = load_dataframe(filepath)
+
+    # Set variability to zero for resources not in the file
+    ensure_column_zeros!(vre_stor, all_resources)
 
     # Reorder DataFrame to R_ID order (order provided in Vre_and_stor_data.csv)
-    select!(vre_stor_solar, [:Time_Index; Symbol.(all_resources)])
-    select!(vre_stor_wind, [:Time_Index; Symbol.(all_resources)])
+    select!(vre_stor, [:Time_Index; Symbol.(all_resources)])
 
     # Maximum power output and variability of each energy resource
-    inputs["pP_Max_Solar"] = transpose(Matrix{Float64}(vre_stor_solar[1:inputs["T"],
-        2:(inputs["G"] + 1)]))
-    inputs["pP_Max_Wind"] = transpose(Matrix{Float64}(vre_stor_wind[1:inputs["T"],
-        2:(inputs["G"] + 1)]))
+    inputs["pP_Max" * "_" * maxpower_key] = transpose(Matrix{Float64}(vre_stor[
+        1:inputs["T"], 2:(inputs["G"] + 1)]))
 
-    println(filename1 * " Successfully Read!")
-    println(filename2 * " Successfully Read!")
+    println(filepath * " Successfully Read!")
+end
+
+function ensure_column_zeros!(vre_stor_df, all_resources)
+    existing_variability = names(vre_stor_df)
+    for r in all_resources
+        if r ∉ existing_variability
+            ensure_column!(vre_stor_df, r, 0.0)
+        end
+    end
 end
