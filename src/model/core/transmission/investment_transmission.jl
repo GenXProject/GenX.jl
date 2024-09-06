8205e66e9650c86642e4ba75243867ebeb73d8a0
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2024-09-06 11:40:49 -0400

    Update investment_transmission.jl
    Fixed rendering of transmission reinforcement cost in the text portion of the doc

--- src/model/core/transmission/investment_transmission.jl
+++ src/model/core/transmission/investment_transmission.jl
@@ -1,6 +1,6 @@
 @doc raw"""
     function investment_transmission!(EP::Model, inputs::Dict, setup::Dict)
-This function model transmission expansion and adds transmission reinforcement or construction costs to the objective function. Transmission reinforcement costs are equal to the sum across all lines of the product between the transmission reinforcement/construction cost, $pi^{TCAP}_{l}$, times the additional transmission capacity variable, $\bigtriangleup\varphi^{cap}_{l}$.
+This function model transmission expansion and adds transmission reinforcement or construction costs to the objective function. Transmission reinforcement costs are equal to the sum across all lines of the product between the transmission reinforcement/construction cost, $\pi^{TCAP}_{l}$, times the additional transmission capacity variable, $\bigtriangleup\varphi^{cap}_{l}$.
 ```math
 \begin{aligned}
     & \sum_{l \in \mathcal{L}}\left(\pi^{TCAP}_{l} \times \bigtriangleup\varphi^{cap}_{l}\right)
