012a28d7a7a7b46407f157a7f4119f47d6adbff3
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2024-12-04 15:22:34 -0500

    Fix typos in documentation (#768)
    Co-authored-by: lbonaldo <bonaldo.luca12@gmail.com>

--- src/model/core/transmission/investment_transmission.jl
+++ src/model/core/transmission/investment_transmission.jl
@@ -1,13 +1,15 @@
 @doc raw"""
-    function investment_transmission!(EP::Model, inputs::Dict, setup::Dict)
-This function model transmission expansion and adds transmission reinforcement or construction costs to the objective function. Transmission reinforcement costs are equal to the sum across all lines of the product between the transmission reinforcement/construction cost, $pi^{TCAP}_{l}$, times the additional transmission capacity variable, $\bigtriangleup\varphi^{cap}_{l}$.
+    investment_transmission!(EP::Model, inputs::Dict, setup::Dict)
+This function model transmission expansion and adds transmission reinforcement or construction costs to the objective function. Transmission reinforcement costs are equal to the sum across all lines of the product between the transmission reinforcement/construction cost, $\pi^{TCAP}_{l}$, times the additional transmission capacity variable, $\bigtriangleup\varphi^{cap}_{l}$.
 ```math
 \begin{aligned}
     & \sum_{l \in \mathcal{L}}\left(\pi^{TCAP}_{l} \times \bigtriangleup\varphi^{cap}_{l}\right)
 \end{aligned}
 ```
 Note that fixed O\&M and replacement capital costs (depreciation) for existing transmission capacity is treated as a sunk cost and not included explicitly in the GenX objective function.
+
 **Accounting for Transmission Between Zones**
+
 Available transmission capacity between zones is set equal to the existing line's maximum power transfer capacity, $\overline{\varphi^{cap}_{l}}$, plus any transmission capacity added on that line (for lines eligible for expansion in the set $\mathcal{E}$). 
 ```math
 \begin{aligned}
