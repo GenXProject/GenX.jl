057c12f3257cebe9e05fd03251549c1d173cb1ab
Nick Laws <laws.nick@gmail.com>
2024-12-14 12:47:09 -0700

    Update test_market.jl

--- test/test_market.jl
+++ test/test_market.jl
@@ -1,21 +1,27 @@
 module TestMarket
 
 using Test
-
 include(joinpath(@__DIR__, "utilities.jl"))
 
-# obj_true = 395.171391
+
 test_path = "market"
 
-# Define test inputs
+# Get baseline results to test against
+EP_no_market, _, _ = redirect_stdout(devnull) do
+    run_genx_case_testing(test_path, Dict())
+end
+base_line_cost = JuMP.objective_value(EP_no_market)
+base_line_LMPs = JuMP.dual.(EP_no_market[:cPowerBalance])
+
+
 genx_setup = Dict(
     "Market" => 1
 )
 
 # We run several tests with different market prices by copying the market_price_scenarios into
 # system/Market_data.csv.
-scenarios_path = joinpath(@__DIR__, "market", "market_price_scenarios")
-market_data_path = joinpath(@__DIR__, "market", "system", "Market_data.csv")
+scenarios_path = joinpath(@__DIR__, test_path, "market_price_scenarios")
+market_data_path = joinpath(@__DIR__, test_path, "system", "Market_data.csv")
 
 price_csvs = [
     joinpath(scenarios_path, "one_tier_30.csv")
@@ -28,18 +34,22 @@ for price_csv in price_csvs
     EP, inputs, _ = redirect_stdout(devnull) do
         run_genx_case_testing(test_path, genx_setup)
     end
+    LMPs = JuMP.dual.(EP[:cPowerBalance])
 
     # $30/MWh with 1 MW market limit means that every hour 1 MWh is purchased
+    # savings are the difference of sum(base_line_LMPs) and 8760 MWh * $30/MWh
     if endswith(price_csv, "one_tier_30.csv")
+        market_costs = JuMP.value(EP[:eMarketPurchasesCost])
         @test all(price ≈ 30.0 for price in inputs[GenX.MARKET_PRICES][1])
-        @test JuMP.value(EP[:eMarketPurchasesCost]) / inputs[GenX.MARKET_PRICES][1][1] ≈ 8760.0
+        @test market_costs / inputs[GenX.MARKET_PRICES][1][1] ≈ 8760.0
         @test all((
-            purchase ≈ inputs[GenX.MARKET_LIMITS][1] for purchase in JuMP.value.(EP[:vMarketPurchaseMW])
+            purchase ≈ inputs[GenX.MARKET_LIMITS][1] 
+            for purchase in JuMP.value.(EP[:vMarketPurchaseMW])
         ))
+        savings = sum(base_line_LMPs) - market_costs
+        @test JuMP.objective_value(EP) + savings ≈ base_line_cost
     end
-
-    obj_test = objective_value(EP)
-    
+   
     rm(market_data_path)
 end
 
