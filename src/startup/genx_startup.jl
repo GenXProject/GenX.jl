6bfad64fe9fe4a6c3412e20d8c8362f8358d20f2
Nick Laws <laws.nick@gmail.com>
2025-01-04 11:04:27 -0700

    Revert "remove precompile step"
    This reverts commit ea956f5ea053bd22327cc7cc06bdf41a3293e1c3.

--- src/startup/genx_startup.jl
+++ src/startup/genx_startup.jl
@@ -32,3 +32,51 @@ function get_project_version(project_file::String)
     isnothing(v) && return nothing
     return VersionNumber(v::AbstractString)
 end
+
+"""
+    precompile()
+
+Precompiles the functions `run_genx_case!` and `cluster_inputs` with specific 
+arguments. This function is intended to speed up the first use of GenX in a 
+new Julia session by precompiling it. 
+
+The function redirects standard output to `devnull` to suppress any output 
+generated during the precompilation process, and sets up a logger to capture 
+any warnings or errors.
+
+# Output
+Returns `nothing`.
+
+"""
+function _precompile()
+    @info "Running precompile script for GenX. This may take a few minutes."
+    redirect_stdout(devnull) do
+        warnerror_logger = ConsoleLogger(stderr, Logging.Warn)
+        with_logger(warnerror_logger) do
+            @compile_workload begin
+                case = joinpath(pkgdir(GenX), "precompile/case")
+                _precompile_tdr(case)   # Precompile TDR
+                run_genx_case!(case, HiGHS.Optimizer)   # Precompile run_genx_case!
+            end
+        end
+    end
+    isdir("precompile/case/results") &&
+        rm("precompile/case/results"; force = true, recursive = true)   # Clean up
+    return nothing
+end
+
+function _precompile_tdr(case)
+    settings_path = get_settings_path(case)
+    setup = configure_settings(get_settings_path(case, "genx_settings.yml"),
+        get_settings_path(case, "output_settings.yml"))
+    setup["TimeDomainReduction"] = 1    # Enable TDR for precompilation
+    cluster_inputs(case, settings_path, setup)
+    isdir("precompile/case/TDR_results") &&
+        rm("precompile/case/TDR_results"; force = true, recursive = true)   # Clean up
+    return nothing
+end
+
+# Precompile `run_genx_case!` and `TDR` unless the environment variable `GENX_PRECOMPILE` is set to `false`
+if get(ENV, "GENX_PRECOMPILE", "true") != "false"
+    _precompile()
+end
