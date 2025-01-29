4a08eff5592d14fdc069d65b198e59cee1050f80
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2025-01-29 10:27:48 -0500

    Update thermal.jl with links to fuels page for Piecewise linear cost curve
    This PR creates links from the Thermal page to the fuels page for piecewise linear cost curve

--- src/model/resources/thermal/thermal.jl
+++ src/model/resources/thermal/thermal.jl
@@ -2,6 +2,7 @@
     thermal!(EP::Model, inputs::Dict, setup::Dict)
 The thermal module creates decision variables, expressions, and constraints related to thermal power plants e.g. coal, oil or natural gas steam plants, natural gas combined cycle and combustion turbine plants, nuclear, hydrogen combustion etc.
 This module uses the following 'helper' functions in separate files: ```thermal_commit()``` for thermal resources subject to unit commitment decisions and constraints (if any) and ```thermal_no_commit()``` for thermal resources not subject to unit commitment (if any).
+For the piecewise linear cost function for thermal generators, please refer to the documentation in the \href[fuels][https://genxproject.github.io/GenX.jl/stable/Model_Reference/core/#GenX.fuel!-Tuple%7BJuMP.Model,%20Dict,%20Dict%7D] page
 """
 function thermal!(EP::Model, inputs::Dict, setup::Dict)
     gen = inputs["RESOURCES"]
