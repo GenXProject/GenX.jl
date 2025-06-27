f733961b7e90cfe5b2b714e1d95178738d42cbac
wilson-ricks <67025245+wilson-ricks@users.noreply.github.com>
2025-06-26 17:54:27 -0700

    Fix_electrolyzer_test_final
    Corrected the settings in the electrolyzer test to activate hourly matching

--- test/test_electrolyzer.jl
+++ test/test_electrolyzer.jl
@@ -12,8 +12,7 @@ genx_setup = Dict("Trans_Loss_Segments" => 1,
     "UCommit" => 2,
     "StorageLosses" => 1,
     "ParameterScale" => 1,
-    "HourlyMatching" => 1,
-    "HydrogenHourlyMatching" => 1,
+    "HourlyMatchingRequirement" => 1,
     "HydrogenMinimumProduction" => 1
 )
 settings = GenX.default_settings()
