d8448149af7c4ebdc6ebcd945c442a9579adc982
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2025-11-17 12:59:33 -0500

    Updates to Nomenclature of Output Files
    This PR attempts at a better, more intuitive, and logical nomenclature for out files as well as establishes naming conventions and guidelines for future new output files.

--- src/write_outputs/capacity_reserve_margin/write_reserve_margin.jl
+++ src/write_outputs/capacity_reserve_margin/write_reserve_margin.jl
@@ -1,9 +1,9 @@
 function write_reserve_margin(path::AbstractString, setup::Dict, EP::Model)
-    temp_ResMar = dual.(EP[:cCapacityResMargin])
+    temp_ResMar_Price = dual.(EP[:cCapacityResMargin])
     if setup["ParameterScale"] == 1
-        temp_ResMar = temp_ResMar * ModelScalingFactor # Convert from MillionUS$/GWh to US$/MWh
+        temp_ResMar_Price = temp_ResMar_Price * ModelScalingFactor # Convert from MillionUS$/GWh to US$/MWh
     end
-    dfResMar = DataFrame(temp_ResMar, :auto)
-    CSV.write(joinpath(path, "ReserveMargin.csv"), dfResMar)
+    dfResMarPrice = DataFrame(temp_ResMar_Price, :auto)
+    CSV.write(joinpath(path, "ReserveMarginPrice.csv"), dfResMarPrice)
     return nothing
 end
