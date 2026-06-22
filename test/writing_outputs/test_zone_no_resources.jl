ff14589cb311603e6dbc05f98a0521d23df3849d
David Cole <dlcdavidc@gmail.com>
2026-06-22 15:30:04 -0400

    Trying parameter scale = 1

--- test/writing_outputs/test_zone_no_resources.jl
+++ test/writing_outputs/test_zone_no_resources.jl
@@ -40,7 +40,7 @@ end
 
 function test_case()
     test_path = joinpath(@__DIR__, "zone_no_resources")
-    obj_true = 5.1773638153e12
+    obj_true = 5.1773638153e6
     costs_true = prepare_costs_true()
 
     # Define test setup
@@ -49,7 +49,8 @@ function test_case()
         "UCommit" => 2,
         "CO2Cap" => 2,
         "StorageLosses" => 1,
-        "WriteShadowPrices" => 1)
+        "WriteShadowPrices" => 1, 
+        "ParameterScale" => 1)
 
     # Run the case and get the objective value and tolerance
     EP, inputs, _ = redirect_stdout(devnull) do
